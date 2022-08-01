a99100c98de6ccbcd318dd386b1728658f7eca96
toras9000 <toras9000@example.com>
2022-08-02 01:04:00 +0900

    並列処理用拡張メソッド追加

---
+++ tests/Extensions/EnumerableDataExtensions_PrallelAsync_Tests.cs
@@ -0,0 +1,72 @@
+﻿using System.Reactive.Disposables;
+using FluentAssertions;
+
+namespace LestalyTest.Extensions;
+
+[TestClass()]
+public class EnumerableDataExtensions_PrallelAsync_Tests
+{
+    [TestMethod]
+    public async Task ParallelAsync_ParallelNum()
+    {
+        var actives = new HashSet<int>();
+        var history = new List<int>();
+        IDisposable makePeriod(int n)
+        {
+            lock (actives!)
+            {
+                actives.Add(n);
+                history!.Add(actives.Count);
+            }
+            return Disposable.Create(() =>
+            {
+                lock (actives)
+                {
+                    actives.Remove(n);
+                    history.Add(actives.Count);
+                }
+            });
+        }
+
+        await Enumerable.Range(0, 10)
+            .ToParallelAsync(parallels: 4, ordered: false, async n =>
+            {
+                using var period = makePeriod(n);
+                await Task.Delay(100);
+                return n;
+            })
+            .ToArrayAsync();
+
+        history.Max().Should().Be(4);
+    }
+
+    [TestMethod]
+    public async Task ParallelAsync_NoOrder()
+    {
+        var source = Enumerable.Range(0, 10).ToArray();
+        var results = await source
+            .ToParallelAsync(parallels: 4, ordered: false, async n =>
+            {
+                await Task.Delay(1100 - (n * 100));
+                return n;
+            })
+            .ToArrayAsync();
+
+        results.Should().NotEqual(source);
+    }
+
+    [TestMethod]
+    public async Task ParallelAsync_Ordered()
+    {
+        var source = Enumerable.Range(0, 10).ToArray();
+        var results = await source
+            .ToParallelAsync(parallels: 4, ordered: true, async n =>
+            {
+                await Task.Delay(1100 - (n * 100));
+                return n;
+            })
+            .ToArrayAsync();
+
+        results.Should().Equal(source);
+    }
+}
