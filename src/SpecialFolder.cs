efdb35fa1dc9da40742c3a271e0c0e56680e3be2
toras9000 <toras9000@example.com>
2022-08-28 21:57:29 +0900

    特殊ディレクトリ情報取得メソッド

---
+++ src/SpecialFolder.cs
@@ -0,0 +1,24 @@
+﻿namespace Lestaly;
+
+/// <summary>
+/// 特殊ディレクトリに関する補助メソッド
+/// </summary>
+public static class SpecialFolder
+{
+    /// <summary>指定の種別の特殊ディレクトリ情報を取得する。</summary>
+    /// <param name="kind">特殊フォルダ種別</param>
+    /// <returns>ディレクトリ情報</returns>
+    public static DirectoryInfo Get(Environment.SpecialFolder kind) => new DirectoryInfo(Environment.GetFolderPath(kind));
+
+    /// <summary>ユーザプロファイルディレクトリ情報を取得する。</summary>
+    /// <returns>ディレクトリ情報</returns>
+    public static DirectoryInfo UserProfile() => Get(Environment.SpecialFolder.UserProfile);
+
+    /// <summary>現在のユーザのアプリケーションデータディレクトリ情報を取得する。</summary>
+    /// <returns>ディレクトリ情報</returns>
+    public static DirectoryInfo ApplicationData() => Get(Environment.SpecialFolder.ApplicationData);
+
+    /// <summary>テンポラリディレクトリ情報を取得する。</summary>
+    /// <returns>ディレクトリ情報</returns>
+    public static DirectoryInfo Temporary() => new DirectoryInfo(Path.GetTempPath());
+}
