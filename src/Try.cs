e633628b9b48f9a1549c3986bcd0428385eef806
toras9000 <toras9000@example.com>
2022-10-16 01:34:47 +0900

    例外抑制実行クラス更新

--- src/Try.cs
+++ src/Try.cs
@@ -7,55 +7,111 @@ public static class Try
 {
     /// <summary>処理を実行する</summary>
     /// <param name="action">何らかの処理</param>
-    /// <returns>例外なく実行されたら true を返却。</returns>
-    public static bool Action(Action action)
+    /// <returns>例外が発生した場合はその例外オブジェクト。発生しなければ null を返却。</returns>
+    public static Exception? Action(Action action)
     {
         if (action != null)
         {
-            try { action(); return true; }
-            catch { }
+            try { action.Invoke(); }
+            catch (Exception ex) { return ex; }
         }
-        return false;
+        return null;
+    }
+
+    /// <summary>処理を実行する</summary>
+    /// <param name="action">何らかの処理</param>
+    /// <param name="alternater">例外発生時の代替処理</param>
+    public static void Action(Action action, Action<Exception> alternater)
+    {
+        if (action != null)
+        {
+            try { action.Invoke(); }
+            catch (Exception ex) { if (alternater != null) try { alternater(ex); } catch { } }
+        }
     }
 
     /// <summary>非同期処理を実行する</summary>
     /// <param name="action">何らかの処理</param>
-    /// <returns>例外なく実行されたら true を返却</returns>
-    public static async Task<bool> ActionAsync(Func<ValueTask> action)
+    /// <returns>例外が発生した場合はそのオブジェクト。発生しなければ null を返却。するタスク</returns>
+    public static async Task<Exception?> ActionAsync(Func<ValueTask> action)
     {
         if (action != null)
         {
-            try { await action().ConfigureAwait(false); return true; }
-            catch { }
+            try { await action().ConfigureAwait(false); }
+            catch (Exception ex) { return ex; }
         }
-        return false;
+        return null;
+    }
+
+    /// <summary>非同期処理を実行する</summary>
+    /// <param name="action">何らかの処理</param>
+    /// <param name="alternater">例外発生時の代替処理</param>
+    public static async Task ActionAsync(Func<ValueTask> action, Func<Exception, ValueTask> alternater)
+    {
+        if (action != null)
+        {
+            try { await action().ConfigureAwait(false); }
+            catch (Exception ex) { if (alternater != null) try { await alternater(ex); } catch { } }
+        }
     }
 
     /// <summary>処理を実行して結果値を得る</summary>
-    /// <typeparam name="T">戻り値型</typeparam>
+    /// <typeparam name="TResult">戻り値型</typeparam>
     /// <param name="action">値を得る処理</param>
-    /// <returns>例外なく実行されたら処理の結果値。例外発生時は型のデフォルト値。</returns>
-    public static T? Func<T>(Func<T> action)
+    /// <param name="alternate">処理で値を得られなかった場合の代替値</param>
+    /// <returns>処理結果値または代替値、および例外発生時はその例外オブジェクト(未発生の場合はnull)。</returns>
+    public static (TResult? value, Exception?) Func<TResult>(Func<TResult?> action, TResult? alternate = default)
+    {
+        if (action != null)
+        {
+            try { return (action(), null); }
+            catch (Exception ex) { return (alternate, ex); }
+        }
+        return (alternate, null);
+    }
+
+    /// <summary>処理を実行して結果値を得る</summary>
+    /// <typeparam name="TResult">戻り値型</typeparam>
+    /// <param name="action">値を得る処理</param>
+    /// <param name="alternater">例外発生時の代替処理</param>
+    /// <returns>処理結果値または代替値。</returns>
+    public static TResult? Func<TResult>(Func<TResult?> action, Func<Exception, TResult?> alternater)
     {
         if (action != null)
         {
             try { return action(); }
-            catch { }
+            catch (Exception ex) { if (alternater != null) try { return alternater(ex); } catch { } }
         }
-        return default(T?);
+        return default;
     }
 
     /// <summary>非同期処理を実行して結果値を得る</summary>
-    /// <typeparam name="T">戻り値型</typeparam>
+    /// <typeparam name="TResult">戻り値型</typeparam>
     /// <param name="action">値を得る処理</param>
+    /// <param name="alternate">処理で値を得られなかった場合の代替値</param>
     /// <returns>例外なく実行されたら処理の結果値。例外発生時は型のデフォルト値。</returns>
-    public static async Task<T?> FuncAsync<T>(Func<ValueTask<T>> action)
+    public static async Task<(TResult? value, Exception? error)> FuncAsync<TResult>(Func<ValueTask<TResult>> action, TResult? alternate = default)
+    {
+        if (action != null)
+        {
+            try { return (await action().ConfigureAwait(false), null); }
+            catch (Exception ex) { return (alternate, ex); }
+        }
+        return (alternate, null);
+    }
+
+    /// <summary>非同期処理を実行して結果値を得る</summary>
+    /// <typeparam name="TResult">戻り値型</typeparam>
+    /// <param name="action">値を得る処理</param>
+    /// <param name="alternater">例外発生時の代替処理</param>
+    /// <returns>例外なく実行されたら処理の結果値。例外発生時は型のデフォルト値。</returns>
+    public static async Task<TResult?> FuncAsync<TResult>(Func<ValueTask<TResult>> action, Func<Exception, ValueTask<TResult?>> alternater)
     {
         if (action != null)
         {
             try { return await action().ConfigureAwait(false); }
-            catch { }
+            catch (Exception ex) { if (alternater != null) try { return await alternater(ex); } catch { } }
         }
-        return default(T?);
+        return default;
     }
 }
---
+++ tests/TryTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using Moq;
+
+namespace Lestaly.Tests;
+
+[TestClass()]
+public class TryTests
+{
+    [TestMethod()]
+    public void Action()
+    {
+        FluentActions.Invoking(() => Try.Action(() => { }))
+            .Should().NotThrow().Which.Should().BeNull();
+
+        FluentActions.Invoking(() => Try.Action(() => throw new InvalidDataException("aaa")))
+            .Should().NotThrow().Which.Should().BeOfType<InvalidDataException>();
+
+        var alternater = new Mock<Action<Exception>>();
+
+        FluentActions.Invoking(() => Try.Action(() => { }, alternater.Object)).Should().NotThrow();
+        alternater.Verify(m => m(It.IsAny<Exception>()), Times.Never());
+
+        FluentActions.Invoking(() => Try.Action(() => throw new InvalidDataException("aaa"), alternater.Object)).Should().NotThrow();
+        alternater.Verify(m => m(It.IsAny<InvalidDataException>()), Times.Once());
+    }
+
+    [TestMethod()]
+    public async Task ActionAsync()
+    {
+        (await FluentActions.Awaiting(() => Try.ActionAsync(() => ValueTask.CompletedTask)).Should().NotThrowAsync())
+            .Which.Should().BeNull();
+
+        (await FluentActions.Awaiting(() => Try.ActionAsync(() => throw new InvalidDataException("aaa"))).Should().NotThrowAsync())
+            .Which.Should().BeOfType<InvalidDataException>();
+
+        var alternater = new Mock<Func<Exception, ValueTask>>();
+
+        await FluentActions.Awaiting(() => Try.ActionAsync(() => ValueTask.CompletedTask, alternater.Object)).Should().NotThrowAsync();
+        alternater.Verify(m => m(It.IsAny<Exception>()), Times.Never());
+
+        await FluentActions.Awaiting(() => Try.ActionAsync(() => throw new InvalidDataException("aaa"), alternater.Object)).Should().NotThrowAsync();
+        alternater.Verify(m => m(It.IsAny<InvalidDataException>()), Times.Once());
+    }
+
+    [TestMethod()]
+    public void Func()
+    {
+        FluentActions.Invoking(() => Try.Func(() => 1, 5))
+            .Should().NotThrow().Which.Should().Be((1, null));
+
+        var err = new InvalidDataException("aaa");
+        FluentActions.Invoking(() => Try.Func(() => throw err, 5))
+            .Should().NotThrow().Which.Should().Be((5, err));
+
+        FluentActions.Invoking(() => Try.Func(() => 1, ex => 9))
+            .Should().NotThrow().Which.Should().Be(1);
+
+        FluentActions.Invoking(() => Try.Func<int>(() => throw new InvalidDataException("aaa"), ex => 9))
+            .Should().NotThrow().Which.Should().Be(9);
+    }
+
+    [TestMethod()]
+    public async Task FuncAsync()
+    {
+        (await FluentActions.Awaiting(() => Try.FuncAsync(() => ValueTask.FromResult(1))).Should().NotThrowAsync())
+            .Which.Should().Be((1, null));
+
+        var err = new InvalidDataException("aaa");
+        (await FluentActions.Awaiting(() => Try.FuncAsync(() => throw err, 5)).Should().NotThrowAsync())
+            .Which.Should().Be((5, err));
+
+        (await FluentActions.Awaiting(() => Try.FuncAsync(() => ValueTask.FromResult(2), ex => ValueTask.FromResult(7))).Should().NotThrowAsync())
+            .Which.Should().Be(2);
+
+        (await FluentActions.Awaiting(() => Try.FuncAsync(() => throw err, ex => ValueTask.FromResult(7))).Should().NotThrowAsync())
+            .Which.Should().Be(7);
+    }
+
+}
