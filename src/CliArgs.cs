c26933c26cd15ac264de3a16948de0f137410f4d
toras9000 <toras9000@example.com>
2022-12-30 22:53:56 +0900

    コマンドライン引数用補助処理

---
+++ src/CliArgs.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Lestaly;
+
+/// <summary>
+/// コマンドライン引数処理
+/// </summary>
+public static class CliArgs
+{
+    /// <summary>コマンドライン引数を CommandLineParser でオブジェクトにマッピングする。</summary>
+    /// <typeparam name="T">マッピング対象型</typeparam>
+    /// <param name="args">コマンドライン引数</param>
+    /// <returns>マップされた型のインスタンス</returns>
+    public static T Parse<T>(IEnumerable<string> args)
+    {
+        return Parse<T>(args, settings =>
+        {
+            settings.IgnoreUnknownArguments = false;
+            settings.AutoVersion = false;
+        });
+    }
+
+    /// <summary>コマンドライン引数をCommandLineParser でオブジェクトにマッピングする。</summary>
+    /// <typeparam name="T">マッピング対象型</typeparam>
+    /// <param name="args">コマンドライン引数</param>
+    /// <param name="configuration">パーサオプション構成デリゲート</param>
+    /// <returns>マップされた型のインスタンス</returns>
+    public static T Parse<T>(IEnumerable<string> args, Action<ParserSettings> configuration)
+    {
+        var parser = new Parser(configuration);
+        var result = parser.ParseArguments<T>(args);
+        if (result == null) throw new PavedMessageException("Argument error", fatal: false);
+
+        var firstError = result.Errors.FirstOrDefault();
+        if (firstError != null)
+        {
+            var builder = SentenceBuilder.Create();
+            var sentence = builder.FormatError(firstError);
+            throw new PavedMessageException("Argument error: " + sentence, fatal: false);
+        }
+
+        return result.Value ?? throw new PavedMessageException("Argument error", fatal: false);
+    }
+
+}
---
+++ tests/CliArgsTests.cs
@@ -0,0 +1,55 @@
+﻿using CommandLine;
+using FluentAssertions;
+using Moq;
+
+namespace Lestaly.Tests;
+
+[TestClass()]
+public class CliArgsTests
+{
+    private class Options
+    {
+        [Value(0)]
+        public string? Target { get; set; }
+
+        [Option('p', "parameter")]
+        public string? Param { get; set; }
+
+    }
+
+    [TestMethod()]
+    public void Parse()
+    {
+        {
+            var arguments = new[] { "def", };
+            var options = CliArgs.Parse<Options>(arguments);
+            options.Target.Should().Be("def");
+            options.Param.Should().BeNull();
+        }
+
+        {
+            var arguments = new[] { "--parameter", "asd", "def" };
+            var options = CliArgs.Parse<Options>(arguments);
+            options.Target.Should().Be("def");
+            options.Param.Should().Be("asd");
+        }
+
+        {
+            var arguments = new[] { "--parameter", "asd", };
+            var options = CliArgs.Parse<Options>(arguments);
+            options.Target.Should().BeNull();
+            options.Param.Should().Be("asd");
+        }
+
+    }
+
+    [TestMethod()]
+    public void Parse_Error()
+    {
+        {
+            var arguments = new[] { "--tekito", "aaa" };
+            new Action(() => CliArgs.Parse<Options>(arguments))
+                .Should().Throw<Exception>();
+        }
+    }
+}
