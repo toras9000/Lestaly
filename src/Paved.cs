6d3f94ca00da297aa5e409b1983f4e40a18f9f4e
toras9000 <toras9000@example.com>
2023-01-03 14:06:48 +0900

    エラー補足処理のメッセージ種別

--- src/Paved.cs
+++ src/Paved.cs
@@ -1,70 +1,6 @@
 ﻿namespace Lestaly;
 
 /// <summary>
-/// 実行補助オプション
-/// </summary>
-public class PavedOptions<T>
-{
-    /// <summary>エラー発生時に一時停止するか否か</summary>
-    public bool PauseOnError { get; set; } = true;
-
-    /// <summary>キャンセル発生時に一時停止するか否か</summary>
-    public bool PauseOnCancel { get; set; } = true;
-
-    /// <summary>処理終了後に一時停止するか否か</summary>
-    public bool PauseOnExit { get; set; } = false;
-
-    /// <summary>一時停止時メッセージ</summary>
-    public string? PauseMessage { get; set; } = null;
-
-    /// <summary>エラー時ハンドラ</summary>
-    public Func<Exception, T>? ErrorHandler { get; set; } = null;
-
-    #region 設定処理
-    /// <summary>一時停止無しに設定する。</summary>
-    public PavedOptions<T> NoPause()
-    {
-        this.PauseOnError = false;
-        this.PauseOnExit = false;
-        this.PauseOnCancel = false;
-        return this;
-    }
-
-    /// <summary>いずれかの要因による一時停止ありに設定する。</summary>
-    public PavedOptions<T> AnyPause()
-    {
-        this.PauseOnError = true;
-        this.PauseOnExit = true;
-        this.PauseOnCancel = true;
-        return this;
-    }
-    #endregion
-}
-
-/// <summary>
-/// 実行補助処理で特別に扱うエラーメッセージ伝達用例外
-/// </summary>
-public class PavedMessageException : Exception
-{
-    /// <summary>エラーメッセージを指定するコンストラクタ</summary>
-    /// <param name="message">エラーメッセージ</param>
-    public PavedMessageException(string message) : base(message) { this.Fatal = true; }
-
-    /// <summary>エラーメッセージと内部例外を指定するコンストラクタ</summary>
-    /// <param name="message">エラーメッセージ</param>
-    /// <param name="innerException">内部例外</param>
-    public PavedMessageException(string message, Exception innerException) : base(message, innerException) { this.Fatal = true; }
-
-    /// <summary>エラーメッセージと重大度を指定するコンストラクタ</summary>
-    /// <param name="message">エラーメッセージ</param>
-    /// <param name="fatal">致命的なエラーであるか否か。Pavedでのエラーメッセージ色を決定するために使用。</param>
-    public PavedMessageException(string message, bool fatal) : base(message) { this.Fatal = fatal; }
-
-    /// <summary>致命的エラーであるか否か</summary>
-    public bool Fatal { get; }
-}
-
-/// <summary>
 /// 主にスクリプト用の定型実行補助クラス
 /// </summary>
 public static class Paved
@@ -112,7 +48,20 @@ public static class Paved
             }
             else if (ex is PavedMessageException pex)
             {
-                ConsoleWig.WriteLineColord(pex.Fatal ? ConsoleColor.Red : ConsoleColor.Yellow, pex.Message);
+                switch (pex.Kind)
+                {
+                case PavedMessageKind.Error:
+                    ConsoleWig.WriteLineColord(ConsoleColor.Red, pex.Message);
+                    break;
+                case PavedMessageKind.Warning:
+                case PavedMessageKind.Cancelled:
+                    ConsoleWig.WriteLineColord(ConsoleColor.Yellow, pex.Message);
+                    break;
+                case PavedMessageKind.Information:
+                default:
+                    Console.WriteLine(pex.Message);
+                    break;
+                }
             }
             else
             {
@@ -150,7 +99,20 @@ public static class Paved
                 }
                 if (ex is PavedMessageException pex)
                 {
-                    ConsoleWig.WriteLineColord(pex.Fatal ? ConsoleColor.Red : ConsoleColor.Yellow, pex.Message);
+                    switch (pex.Kind)
+                    {
+                    case PavedMessageKind.Error:
+                        ConsoleWig.WriteLineColord(ConsoleColor.Red, pex.Message);
+                        break;
+                    case PavedMessageKind.Warning:
+                    case PavedMessageKind.Cancelled:
+                        ConsoleWig.WriteLineColord(ConsoleColor.Yellow, pex.Message);
+                        break;
+                    case PavedMessageKind.Information:
+                    default:
+                        Console.WriteLine(pex.Message);
+                        break;
+                    }
                 }
                 else
                 {
---
+++ src/PavedData.cs
@@ -0,0 +1,85 @@
+﻿namespace Lestaly;
+
+/// <summary>
+/// 実行補助オプション
+/// </summary>
+public class PavedOptions<T>
+{
+    /// <summary>エラー発生時に一時停止するか否か</summary>
+    public bool PauseOnError { get; set; } = true;
+
+    /// <summary>キャンセル発生時に一時停止するか否か</summary>
+    public bool PauseOnCancel { get; set; } = true;
+
+    /// <summary>処理終了後に一時停止するか否か</summary>
+    public bool PauseOnExit { get; set; } = false;
+
+    /// <summary>一時停止時メッセージ</summary>
+    public string? PauseMessage { get; set; } = null;
+
+    /// <summary>エラー時ハンドラ</summary>
+    public Func<Exception, T>? ErrorHandler { get; set; } = null;
+
+    #region 設定処理
+    /// <summary>一時停止無しに設定する。</summary>
+    public PavedOptions<T> NoPause()
+    {
+        this.PauseOnError = false;
+        this.PauseOnExit = false;
+        this.PauseOnCancel = false;
+        return this;
+    }
+
+    /// <summary>いずれかの要因による一時停止ありに設定する。</summary>
+    public PavedOptions<T> AnyPause()
+    {
+        this.PauseOnError = true;
+        this.PauseOnExit = true;
+        this.PauseOnCancel = true;
+        return this;
+    }
+    #endregion
+}
+
+/// <summary>
+/// メッセージ種別
+/// </summary>
+public enum PavedMessageKind
+{
+    /// <summary>エラー</summary>
+    Error,
+    /// <summary>警告</summary>
+    Warning,
+    /// <summary>中止</summary>
+    Cancelled,
+    /// <summary>情報</summary>
+    Information,
+}
+
+/// <summary>
+/// 実行補助処理で特別に扱うエラーメッセージ伝達用例外
+/// </summary>
+public class PavedMessageException : Exception
+{
+    /// <summary>エラーメッセージを指定するコンストラクタ</summary>
+    /// <param name="message">エラーメッセージ</param>
+    public PavedMessageException(string message) : base(message) { this.Kind = PavedMessageKind.Error; }
+
+    /// <summary>エラーメッセージと内部例外を指定するコンストラクタ</summary>
+    /// <param name="message">エラーメッセージ</param>
+    /// <param name="innerException">内部例外</param>
+    public PavedMessageException(string message, Exception innerException) : base(message, innerException) { this.Kind = PavedMessageKind.Error; }
+
+    /// <summary>エラーメッセージと重大度を指定するコンストラクタ</summary>
+    /// <param name="message">エラーメッセージ</param>
+    /// <param name="fatal">致命的なエラーであるか否か。Pavedでのエラーメッセージ色を決定するために使用。</param>
+    public PavedMessageException(string message, bool fatal) : base(message) { this.Kind = fatal ? PavedMessageKind.Error : PavedMessageKind.Warning; }
+
+    /// <summary>エラーメッセージとメッセージ種別を指定するコンストラクタ</summary>
+    /// <param name="message">エラーメッセージ</param>
+    /// <param name="kind">メッセージの種類。</param>
+    public PavedMessageException(string message, PavedMessageKind kind) : base(message) { this.Kind = kind; }
+
+    /// <summary>メッセージの種類</summary>
+    public PavedMessageKind Kind { get; }
+}
