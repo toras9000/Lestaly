1033dc378a140c250db5acd481fe9d78ad963222
toras9000 <toras9000@example.com>
2023-01-08 10:14:36 +0900

    ファイル/ディレクトリ拡張メソッド追加

--- src/Extensions/FileSystemInfoExtensions.cs
+++ src/Extensions/FileSystemInfoExtensions.cs
@@ -36,4 +36,31 @@ public static class FileSystemInfoExtensions
     }
     #endregion
 
+    #region Path
+    /// <summary>ファイルパスの構成セグメントを取得する。</summary>
+    /// <param name="self">対象ファイルシステムアイテムのFileSystemInfo</param>
+    /// <returns>パス構成セグメントのリスト</returns>
+    public static IList<string> GetPathSegments(this FileSystemInfo self)
+        => CometFlavor.Extensions.IO.FileSystemInfoExtensions.GetPathSegments(self);
+
+    /// <summary>ファイルが指定のディレクトリの子孫であるかを判定する。</summary>
+    /// <remarks></remarks>
+    /// <param name="self">対象ファイルシステムアイテムのFileSystemInfo</param>
+    /// <param name="other">比較するディレクトリ</param>
+    /// <param name="sameIs">同一階層を真とするか否か</param>
+    /// <returns>指定ディレクトリの子孫であるか否か</returns>
+    public static bool IsDescendantOf(this FileSystemInfo self, DirectoryInfo other, bool sameIs = true)
+        => CometFlavor.Extensions.IO.FileSystemInfoExtensions.IsDescendantOf(self, other, sameIs);
+
+    /// <summary>指定のディレクトリを起点としたファイルの相対パスを取得する。</summary>
+    /// <remarks>
+    /// 単純なパス文字列処理であり、リパースポイントなどを解釈することはない。
+    /// </remarks>
+    /// <param name="self">対象ファイルシステムアイテムのFileSystemInfo</param>
+    /// <param name="baseDir">基準ディレクトリのDirectoryInfo</param>
+    /// <param name="ignoreCase">大文字と小文字を同一視するか否か</param>
+    /// <returns>相対パス</returns>
+    public static string RelativePathFrom(this FileSystemInfo self, DirectoryInfo baseDir, bool ignoreCase)
+        => CometFlavor.Extensions.IO.FileSystemInfoExtensions.RelativePathFrom(self, baseDir, ignoreCase);
+    #endregion
 }
