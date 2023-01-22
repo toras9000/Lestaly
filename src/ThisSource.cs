bd30ddaa2a981942824f8b94f006e4995952dc38
toras9000 <toras9000@example.com>
2023-01-22 19:18:57 +0900

    呼び出し元情報取得追加

--- src/ThisSource.cs
+++ src/ThisSource.cs
@@ -1,4 +1,6 @@
-﻿namespace Lestaly;
+﻿using System.Runtime.CompilerServices;
+
+namespace Lestaly;
 
 /// <summary>
 /// 呼び出し元ソースファイルに関する補助メソッド
@@ -8,17 +10,27 @@ public static class ThisSource
     /// <summary>呼び出し元ソースファイルパスを示すファイル情報を取得する。</summary>
     /// <param name="path">この引数は省略する必要がある。</param>
     /// <returns>呼び出し元ソースファイル情報</returns>
-    public static FileInfo GetFile([System.Runtime.CompilerServices.CallerFilePath] string path = "") => new FileInfo(path);
+    public static FileInfo GetFile([CallerFilePath] string path = "") => new FileInfo(path);
 
     /// <summary>呼び出し元ソースファイルからの相対パスで示されるファイル情報を取得する。</summary>
     /// <param name="relativePath">相対パス</param>
     /// <param name="path">この引数は省略する必要がある。</param>
     /// <returns>相対ファイル情報</returns>
-    public static FileInfo GetRelativeFile(string relativePath, [System.Runtime.CompilerServices.CallerFilePath] string path = "") => new FileInfo(Path.Combine(Path.GetDirectoryName(path)!, relativePath));
+    public static FileInfo GetRelativeFile(string relativePath, [CallerFilePath] string path = "") => new FileInfo(Path.Combine(Path.GetDirectoryName(path)!, relativePath));
 
     /// <summary>呼び出し元ソースファイルからの相対パスで示されるディレクトリ情報を取得する。</summary>
     /// <param name="relativePath">相対パス</param>
     /// <param name="path">この引数は省略する必要がある。</param>
     /// <returns>相対ディレクトリ情報</returns>
-    public static DirectoryInfo GetRelativeDirectory(string relativePath, [System.Runtime.CompilerServices.CallerFilePath] string path = "") => new DirectoryInfo(Path.Combine(Path.GetDirectoryName(path)!, relativePath));
+    public static DirectoryInfo GetRelativeDirectory(string relativePath, [CallerFilePath] string path = "") => new DirectoryInfo(Path.Combine(Path.GetDirectoryName(path)!, relativePath));
+
+    /// <summary>呼び出し元行番号を取得する</summary>
+    /// <param name="lineNumber">この引数は省略する必要がある。</param>
+    /// <returns>行番号</returns>
+    public static int GetLineNumber([CallerLineNumber] int lineNumber = 0) => lineNumber;
+
+    /// <summary>呼び出し元メンバー名を取得する</summary>
+    /// <param name="member">この引数は省略する必要がある。</param>
+    /// <returns>メンバー名</returns>
+    public static string GetMemberName([CallerMemberName] string member = "") => member;
 }
