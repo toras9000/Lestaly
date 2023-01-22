e7728258d40b5463633297d97ffa1d4326e0cc02
toras9000 <toras9000@example.com>
2023-01-22 19:18:40 +0900

    文字列の補助メソッド追加

---
+++ src/Extensions/StringEncodeExtensions.cs
@@ -0,0 +1,195 @@
+﻿using System.Buffers;
+using System.Text;
+
+namespace Lestaly;
+
+/// <summary>
+/// string に対するエンコード関係拡張メソッド
+/// </summary>
+public static class StringEncodeExtensions
+{
+    /// <summary>文字列をUTF8バイト列にエンコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>エンコードしたバイト列</returns>
+    public static byte[] EncodeUtf8(this string self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8(self.AsSpan());
+    }
+
+    /// <summary>文字列をUTF8バイト列にエンコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>エンコードしたバイト列</returns>
+    public static byte[] EncodeUtf8(this char[] self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8(self.AsSpan());
+    }
+
+    /// <summary>文字列をUTF8バイト列にエンコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>エンコードしたバイト列</returns>
+    public static byte[] EncodeUtf8(this Span<char> self) => EncodeUtf8((ReadOnlySpan<char>)self);
+
+    /// <summary>文字列をUTF8バイト列にエンコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>エンコードしたバイト列</returns>
+    public static byte[] EncodeUtf8(this ReadOnlySpan<char> self)
+    {
+        var writer = new ArrayBufferWriter<byte>(self.Length);
+        var length = Encoding.UTF8.GetBytes(self, writer);
+        return writer.WrittenSpan.ToArray();
+    }
+
+
+    /// <summary>バイト列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8(this byte[] self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return DecodeUtf8(self.AsSpan());
+    }
+
+    /// <summary>バイト列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8(this Span<byte> self) => DecodeUtf8((ReadOnlySpan<byte>)self);
+
+    /// <summary>バイト列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">バイト列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8(this ReadOnlySpan<byte> self)
+    {
+        return Encoding.UTF8.GetString(self);
+    }
+
+
+    /// <summary>文字列のUTF8バイト列をBase64文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <param name="wrap">エンコードテキストに一定の幅で改行を含めるかどうか</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Base64(this string self, bool wrap = false)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8Base64(self.AsSpan(), wrap);
+    }
+
+    /// <summary>文字列のUTF8バイト列をBase64文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <param name="wrap">エンコードテキストに一定の幅で改行を含めるかどうか</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Base64(this char[] self, bool wrap = false)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8Base64(self.AsSpan(), wrap);
+    }
+
+    /// <summary>文字列のUTF8バイト列をBase64文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <param name="wrap">エンコードテキストに一定の幅で改行を含めるかどうか</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Base64(this Span<char> self, bool wrap = false) => EncodeUtf8Base64((ReadOnlySpan<char>)self, wrap);
+
+    /// <summary>文字列のUTF8バイト列をBase64文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <param name="wrap">エンコードテキストに一定の幅で改行を含めるかどうか</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Base64(this ReadOnlySpan<char> self, bool wrap = false)
+    {
+        var writer = new ArrayBufferWriter<byte>(self.Length / 4 + self.Length);
+        var length = Encoding.UTF8.GetBytes(self, writer);
+        var option = wrap ? Base64FormattingOptions.InsertLineBreaks : Base64FormattingOptions.None;
+        return Convert.ToBase64String(writer.WrittenSpan, option);
+    }
+
+
+    /// <summary>Base64文字列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">Base64文字列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8Base64(this string self)
+    {
+        var bin = Convert.FromBase64String(self);
+        return Encoding.UTF8.GetString(bin);
+    }
+
+
+    /// <summary>文字列のUTF8バイト列をHEX文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Hex(this string self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8Hex(self.AsSpan());
+    }
+
+    /// <summary>文字列のUTF8バイト列をHEX文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Hex(this char[] self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return EncodeUtf8Hex(self.AsSpan());
+    }
+
+    /// <summary>文字列のUTF8バイト列をHEX文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Hex(this Span<char> self) => EncodeUtf8Hex((ReadOnlySpan<char>)self);
+
+    /// <summary>文字列のUTF8バイト列をHEX文字列にエンコードする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>エンコードしたBase64文字列</returns>
+    public static string EncodeUtf8Hex(this ReadOnlySpan<char> self)
+    {
+        var writer = new ArrayBufferWriter<byte>(self.Length / 4 + self.Length);
+        var length = Encoding.UTF8.GetBytes(self, writer);
+        return Convert.ToHexString(writer.WrittenSpan);
+    }
+
+
+    /// <summary>HEX文字列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">Base64文字列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8Hex(this string self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return DecodeUtf8Hex(self.AsSpan());
+    }
+
+    /// <summary>HEX文字列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">Base64文字列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8Hex(this char[] self)
+    {
+        ArgumentNullException.ThrowIfNull(self);
+        return DecodeUtf8Hex(self.AsSpan());
+    }
+
+    /// <summary>HEX文字列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">Base64文字列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8Hex(this Span<char> self) => DecodeUtf8Hex((ReadOnlySpan<char>)self);
+
+    /// <summary>HEX文字列をUTF8として文字列にデコードする。</summary>
+    /// <param name="self">Base64文字列</param>
+    /// <returns>デコードした文字列</returns>
+    public static string DecodeUtf8Hex(this ReadOnlySpan<char> self)
+    {
+        var bin = Convert.FromHexString(self);
+        return Encoding.UTF8.GetString(bin);
+    }
+
+
+    /// <summary>文字列のURIデータ文字列のルールでエスケープする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>エスケープした文字列</returns>
+    public static string EscapeUriData(this string self) => Uri.EscapeDataString(self);
+
+
+    /// <summary>URIデータ文字列のルールでエスケープされた文字列をアンエスケープする。</summary>
+    /// <param name="self">文字列</param>
+    /// <returns>アンエスケープした文字列</returns>
+    public static string UnescapeUriData(this string self) => Uri.UnescapeDataString(self);
+
+}
---
+++ tests/Extensions/StringEncodeExtensionsTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Text;
+using FluentAssertions;
+
+namespace LestalyTest.Extensions;
+
+[TestClass()]
+public class StringEncodeExtensionsTests
+{
+    [TestMethod()]
+    public void EncodeUtf8()
+    {
+        var text = "abcdef";
+
+        var expect = Encoding.UTF8.GetBytes(text);
+
+        text.EncodeUtf8().Should().Equal(expect);
+        text.ToArray().EncodeUtf8().Should().Equal(expect);
+        text.ToArray().AsSpan().EncodeUtf8().Should().Equal(expect);
+        text.AsSpan().EncodeUtf8().Should().Equal(expect);
+    }
+
+    [TestMethod()]
+    public void DecodeUtf8()
+    {
+        var text = "abcdef";
+
+        var encoded = Encoding.UTF8.GetBytes(text);
+
+        encoded.DecodeUtf8().Should().Be(text);
+        encoded.AsSpan().DecodeUtf8().Should().Be(text);
+        encoded.AsSpan().DecodeUtf8().Should().Be(text);
+        ((ReadOnlySpan<byte>)encoded.AsSpan()).DecodeUtf8().Should().Be(text);
+    }
+
+    [TestMethod()]
+    public void EncodeUtf8Base64()
+    {
+        var text = "abcdef";
+
+        var expect = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+
+        text.EncodeUtf8Base64().Should().Be(expect);
+        text.ToArray().EncodeUtf8Base64().Should().Be(expect);
+        text.ToArray().AsSpan().EncodeUtf8Base64().Should().Be(expect);
+        text.AsSpan().EncodeUtf8Base64().Should().Be(expect);
+    }
+
+    [TestMethod()]
+    public void DecodeUtf8Base64()
+    {
+        var text = "abcdef";
+
+        var encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+
+        encoded.DecodeUtf8Base64().Should().Be(text);
+    }
+
+    [TestMethod()]
+    public void EncodeUtf8Hex()
+    {
+        var text = "abcdef";
+
+        var expect = Convert.ToHexString(Encoding.UTF8.GetBytes(text));
+
+        text.EncodeUtf8Hex().Should().Be(expect);
+        text.ToArray().EncodeUtf8Hex().Should().Be(expect);
+        text.ToArray().AsSpan().EncodeUtf8Hex().Should().Be(expect);
+        text.AsSpan().EncodeUtf8Hex().Should().Be(expect);
+    }
+
+    [TestMethod()]
+    public void DecodeUtf8Hex()
+    {
+        var text = "abcdef";
+
+        var encoded = Convert.ToHexString(Encoding.UTF8.GetBytes(text));
+
+        encoded.DecodeUtf8Hex().Should().Be(text);
+        encoded.ToArray().DecodeUtf8Hex().Should().Be(text);
+        encoded.ToArray().AsSpan().DecodeUtf8Hex().Should().Be(text);
+        encoded.AsSpan().DecodeUtf8Hex().Should().Be(text);
+    }
+
+    [TestMethod()]
+    public void EscapeUriData()
+    {
+        var text = "abcdef";
+
+        var expect = Uri.EscapeDataString(text);
+
+        text.EscapeUriData().Should().Be(expect);
+    }
+
+    [TestMethod()]
+    public void UnescapeUriData()
+    {
+        var text = "abcdef";
+
+        var encoded = Uri.EscapeDataString(text);
+
+        encoded.UnescapeUriData().Should().Be(text);
+    }
+
+}
