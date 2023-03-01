1d8fb55a29131973b3eea054fd19630361511462
toras9000 <toras9000@example.com>
2023-03-01 20:34:52 +0900

    カレントディレクトリからのファイルシステム情報取得メソッド修正

--- src/CurrentDir.cs
+++ src/CurrentDir.cs
@@ -14,7 +14,7 @@ public static class CurrentDir
     /// <summary>カレントディレクトリからの相対パスで示されるファイル情報を取得する。</summary>
     /// <param name="relativePath">相対パス</param>
     /// <returns>対象ファイルパスの FileInfo。相対パスが空や空白の場合は例外を送出。</returns>
-    public static FileInfo? RelativeFile(string relativePath)
+    public static FileInfo RelativeFile(string relativePath)
         => string.IsNullOrWhiteSpace(relativePath) ? throw new ArgumentException("Invalid relative path") : new FileInfo(Path.Combine(Environment.CurrentDirectory, relativePath));
 
     /// <summary>カレントディレクトリからの相対パスで示されるディレクトリ情報を取得する。</summary>
@@ -26,6 +26,6 @@ public static class CurrentDir
     /// <summary>カレントディレクトリからの相対パスで示されるディレクトリ情報を取得する。</summary>
     /// <param name="relativePath">相対パス</param>
     /// <returns>対象ディレクトリパスの DirectoryInfo。相対パスが空や空白の場合は基準ディレクトリを返却。</returns>
-    public static DirectoryInfo? RelativeDirectory(string? relativePath)
+    public static DirectoryInfo RelativeDirectory(string? relativePath)
         => new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, relativePath ?? ""));
 }
