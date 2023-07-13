0c12bc14d76b59047d60348aff09f0487ae177ac
toras9000 <toras9000@example.com>
2023-07-14 01:14:23 +0900

    日時の拡張メソッド

---
+++ src/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Lestaly;
+
+/// <summary>
+/// 日時に関するの拡張メソッド
+/// </summary>
+public static class DateTimeExtensions
+{
+    /// <summary>同じ日の時刻が異なる日時を取得する。</summary>
+    /// <param name="self">ベースとなる日時</param>
+    /// <param name="hour">時間</param>
+    /// <param name="minute">分</param>
+    /// <param name="second">秒</param>
+    /// <param name="millisecond">ミリ秒</param>
+    /// <returns>時刻の異なる日時</returns>
+    public static DateTime AtTime(this DateTime self, int hour, int minute, int second = 0, int millisecond = 0)
+    {
+        return new DateTime(self.Year, self.Month, self.Day, hour, minute, second, millisecond, self.Kind);
+    }
+
+    /// <summary>同じ日の時刻が異なる日時を取得する</summary>
+    /// <param name="self">ベースとなる日時</param>
+    /// <param name="time">時刻</param>
+    /// <returns>時刻の異なる日時</returns>
+    public static DateTime AtTime(this DateTime self, TimeOnly time)
+    {
+        return new DateTime(self.Year, self.Month, self.Day, time.Hour, time.Minute, time.Second, time.Millisecond, self.Kind);
+    }
+}
---
+++ tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace LestalyTest.Extensions;
+
+[TestClass()]
+public class DateTimeExtensionsTests
+{
+    [TestMethod()]
+    public void AtTime()
+    {
+        var utc = new DateTime(2023, 4, 5, 6, 7, 8, 9, DateTimeKind.Utc);
+        utc.AtTime(10, 11, 12, 13).Should().Be(new DateTime(2023, 4, 5, 10, 11, 12, 13, DateTimeKind.Utc));
+        utc.AtTime(new TimeOnly(10, 11, 12, 13)).Should().Be(new DateTime(2023, 4, 5, 10, 11, 12, 13, DateTimeKind.Utc));
+
+        var local = new DateTime(2023, 4, 5, 6, 7, 8, 9, DateTimeKind.Local);
+        local.AtTime(10, 11, 12, 13).Should().Be(new DateTime(2023, 4, 5, 10, 11, 12, 13, DateTimeKind.Local));
+        local.AtTime(new TimeOnly(10, 11, 12, 13)).Should().Be(new DateTime(2023, 4, 5, 10, 11, 12, 13, DateTimeKind.Local));
+    }
+}
