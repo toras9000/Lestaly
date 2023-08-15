b8b062a60eea9e98f398d7c48f47e4c8b915b75c
toras9000 <toras9000@example.com>
2023-08-15 15:49:00 +0900

    HTTPクライアント拡張メソッド追加

---
+++ src/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lestaly;
+
+/// <summary>
+/// HttpClient に対する拡張メソッド
+/// </summary>
+public static class HttpClientExtensions
+{
+    /// <summary>GET要求の応答をファイルに保存する。</summary>
+    /// <param name="self">クライアント</param>
+    /// <param name="resource">要求URI</param>
+    /// <param name="path">保存ファイルパス</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>保存先ファイル情報</returns>
+    public static Task<FileInfo> GetFileAsync(this HttpClient self, Uri resource, string path, CancellationToken cancelToken = default)
+        => self.GetFileAsync(resource, new FileInfo(path));
+
+    /// <summary>GET要求の応答をファイルに保存する。</summary>
+    /// <param name="self">クライアント</param>
+    /// <param name="resource">要求URI</param>
+    /// <param name="directory">保存先ディレクトリ</param>
+    /// <param name="name">保存ファイル名</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>保存先ファイル情報</returns>
+    public static Task<FileInfo> GetFileAsync(this HttpClient self, Uri resource, DirectoryInfo directory, string? name = default, CancellationToken cancelToken = default)
+    {
+        var fileName = name ?? resource.Segments.Last();
+        return self.GetFileAsync(resource, directory.RelativeFile(fileName));
+    }
+
+    /// <summary>GET要求の応答をファイルに保存する。</summary>
+    /// <param name="self">クライアント</param>
+    /// <param name="resource">要求URI</param>
+    /// <param name="file">保存先ファイル</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>保存先ファイル情報</returns>
+    public static async Task<FileInfo> GetFileAsync(this HttpClient self, Uri resource, FileInfo file, CancellationToken cancelToken = default)
+    {
+        // HTTP要求
+        using var stream = await self.GetStreamAsync(resource, cancelToken).ConfigureAwait(false);
+
+        // 保存先ファイル作成
+        using var writer = file.WithDirectoryCreate().OpenWrite();
+
+        // ダウンロード内容をファイルに書き込み
+        await stream.CopyToAsync(writer, cancelToken).ConfigureAwait(false);
+
+        return file;
+    }
+}
---
+++ tests/Extensions/HttpClientExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http.Headers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LestalyTest.Extensions;
+
+[TestClass()]
+public class HttpClientExtensionsTests
+{
+    public static WebApplication? TestServer;
+    public static ushort TestPort = 10001;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext)
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.Logging.ClearProviders();
+        TestServer = builder.Build();
+        TestServer.MapGet("/base64/{*encoded}", (string encoded) => Results.Bytes(Convert.FromBase64String(encoded)));
+        TestServer.RunAsync($"http://*:{TestPort}");
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        TestServer?.StopAsync().GetAwaiter().GetResult();
+    }
+
+
+    [TestMethod()]
+    public async Task GetFileAsync()
+    {
+        var tempDir = new TempDir();
+
+        using var client = new HttpClient();
+
+        {
+            var encoded = Convert.ToBase64String(new byte[] { 0x12, 0x34, 0x56, });
+            var uri = new Uri($"http://localhost:{TestPort}/base64/{encoded}");
+            var file = await client.GetFileAsync(uri, tempDir.Info.RelativeFile("aaa"));
+            file.ReadAllBytes().Should().Equal(new byte[] { 0x12, 0x34, 0x56, });
+        }
+        {
+            var encoded = Convert.ToBase64String(new byte[] { 0x34, 0x56, 0x78, });
+            var uri = new Uri($"http://localhost:{TestPort}/base64/{encoded}");
+            var file = await client.GetFileAsync(uri, tempDir.Info.RelativeFile("bbb").FullName);
+            file.ReadAllBytes().Should().Equal(new byte[] { 0x34, 0x56, 0x78, });
+        }
+        {
+            var encoded = Convert.ToBase64String(new byte[] { 0x56, 0x78, 0x9A });
+            var uri = new Uri($"http://localhost:{TestPort}/base64/{encoded}");
+            var file = await client.GetFileAsync(uri, tempDir.Info, "ccc");
+            file.ReadAllBytes().Should().Equal(new byte[] { 0x56, 0x78, 0x9A, });
+        }
+        {
+            var encoded = Convert.ToBase64String(new byte[] { 0x78, 0x9A, 0xBC, });
+            var uri = new Uri($"http://localhost:{TestPort}/base64/{encoded}");
+            var file = await client.GetFileAsync(uri, tempDir.Info);
+            file.ReadAllBytes().Should().Equal(new byte[] { 0x78, 0x9A, 0xBC, });
+        }
+    }
+
+}
