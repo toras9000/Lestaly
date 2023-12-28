36055cde2761a8993f09e1afe2b783ab668b4ed6
toras9000 <toras9000@example.com>
2023-12-29 01:04:20 +0900

    配列プール利用補助構造体

---
+++ src/RentalArray.cs
@@ -0,0 +1,52 @@
+﻿using System.Buffers;
+using System.ComponentModel;
+using DocumentFormat.OpenXml.Math;
+
+namespace Lestaly;
+
+/// <summary>ローカルスコープでプールから配列貸し出しを受けるクラス</summary>
+/// <typeparam name="T">要素型</typeparam>
+/// <remarks>
+/// この構造体はローカルスコープ(スタック上)でプールからの配列貸し出しを管理して自動返却するためのものとなる。
+/// 以下のようにusingを利用してスコープを抜ける際に自動的に返却させる利用を想定している。
+/// <code>
+/// using var array = new RentalArray&lt;byte&gt;(100);
+/// // array.Ephemeral を使うコード
+/// </code>
+/// この構造体はコピーしないでください。
+/// </remarks>
+public ref struct RentalArray<T>
+{
+    /// <summary>コンストラクタ</summary>
+    /// <param name="minimumLength">借り受ける最小サイズ</param>
+    public RentalArray(int minimumLength)
+    {
+        this.rental = ArrayPool<T>.Shared.Rent(minimumLength);
+    }
+
+    /// <summary>貸し出された配列インスタンス</summary>
+    /// <remarks>スコープ内(破棄されるまで)に限定して利用すること。</remarks> 
+    public T[] Instance => this.rental;
+
+    /// <summary>リソースを破棄する</summary>
+    /// <remarks>貸し出しを受けた配列を返却する。</remarks>
+    public void Dispose()
+    {
+        if (this.rental != null)
+        {
+            ArrayPool<T>.Shared.Return(this.rental);
+            this.rental = null!;
+        }
+    }
+
+    /// <summary>使用不可</summary>
+    [Obsolete("Cannot use default constructor", true)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public RentalArray()
+    {
+        throw new NotSupportedException("Cannot use default constructor");
+    }
+
+    /// <summary>貸し出された配列インスタンス</summary>
+    private T[] rental;
+}
---
+++ tests/RentalArrayTests.cs
@@ -0,0 +1,32 @@
+﻿namespace LestalyTest;
+
+
+[TestClass()]
+public class RentalArrayTests
+{
+    [TestMethod()]
+    public void RentReturn()
+    {
+        // 1回貸し出しを受けて配列の参照を取っておき、返却を行う。
+        var first = default(byte[]);
+        using (var array = new RentalArray<byte>(100))
+        {
+            first = array.Instance;
+        }
+
+        // 同じサイズで何度か再貸出しを受けて、同じインスタンスが得られる(つまり上の処理で返却されている)ことを確認する。
+        var rerent = false;
+        for (var i = 0; i < 100; i++)
+        {
+            using var array = new RentalArray<byte>(100);
+            if (object.ReferenceEquals(array.Instance, first))
+            {
+                rerent = true;
+                break;
+            }
+        }
+
+        // ちゃんと返却・再貸出しされたことを確認
+        rerent.Should().BeTrue();
+    }
+}
