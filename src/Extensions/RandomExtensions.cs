2c01f58f210d005309962b6cbcc12f3522e59adf
toras9000 <toras9000@example.com>
2024-01-08 04:40:22 +0900

    ランダム関係の拡張メソッドを追加

---
+++ src/Extensions/RandomExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Lestaly;
+
+/// <summary>
+/// Random に対する拡張メソッド
+/// </summary>
+public static class RandomExtensions
+{
+    /// <summary>ランダム値のバイト配列を作成する。</summary>
+    /// <param name="self">ランダム値を生成するインスタンス</param>
+    /// <param name="length">生成するバイト列の長さ</param>
+    /// <returns>生成したランダム値配列</returns>
+    public static byte[] GetBytes(this Random self, int length)
+    {
+        var data = new byte[length];
+        self.NextBytes(data);
+        return data;
+    }
+}
