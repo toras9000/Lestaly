5d677a4fb2659379f4b108d4e58fcd489c441822
toras9000 <toras9000@example.com>
2024-01-08 18:09:02 +0900

    ストリームに関する拡張メソッド

---
+++ src/Extensions/TextStreamExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System.Buffers;
+
+namespace Lestaly;
+
+/// <summary>
+/// TextReader に対する拡張メソッド
+/// </summary>
+public static class TextStreamExtensions
+{
+    /// <summary>一定のアイドル時間があるまでStreamを読み取る</summary>
+    /// <param name="self">対象TextReader</param>
+    /// <param name="buffer">読み取りデータを格納するバッファ</param>
+    /// <param name="idle">データの読み取りを終了する未受信時間[ms]</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>読み取ったバイト数を得るタスク</returns>
+    public static async ValueTask<int> ReadToIdleAsync(this TextReader self, Memory<char> buffer, int idle, CancellationToken cancelToken = default)
+    {
+        if (idle < 0) throw new ArgumentException($"Invalid {nameof(idle)} time");
+
+        // 読み取りデータ格納先管理
+        var space = buffer;
+
+        while (!space.IsEmpty)
+        {
+            try
+            {
+                // アイドル時間後にキャンセルを行うキャンセルソースを生成
+                using var idleCanceller = CancellationTokenSource.CreateLinkedTokenSource(cancelToken);
+                idleCanceller.CancelAfter(idle);
+
+                // ストリームから読み取り
+                var length = await self.ReadAsync(space, idleCanceller.Token).ConfigureAwait(false);
+                if (length == 0) break;
+
+                // 格納先を残り領域分に更新
+                space = space[length..];
+            }
+            catch (OperationCanceledException) when (!cancelToken.IsCancellationRequested)
+            {
+                break;
+            }
+        }
+
+        // 読み取り済みサイズを算出して返却
+        return buffer.Length - space.Length;
+    }
+
+    /// <summary>一定のアイドル時間があるまでStreamを読み取る</summary>
+    /// <param name="self">対象TextReader</param>
+    /// <param name="writer">読み取ったデータを書き込むバッファライター</param>
+    /// <param name="idle">データの読み取りを終了する未受信時間[ms]</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>読み取ったバイト数を得るタスク</returns>
+    public static async ValueTask<int> ReadToIdleAsync(this TextReader self, IBufferWriter<char> writer, int idle, CancellationToken cancelToken = default)
+    {
+        if (idle < 0) throw new ArgumentException($"Invalid {nameof(idle)} time");
+
+        var total = 0;
+        while (true)
+        {
+            // ライターのバッファを取得
+            var buffer = writer.GetMemory();
+            try
+            {
+                // アイドル時間後にキャンセルを行うキャンセルソースを生成
+                using var idleCanceller = CancellationTokenSource.CreateLinkedTokenSource(cancelToken);
+                idleCanceller.CancelAfter(idle);
+
+                // ストリームから読み取り
+                var length = await self.ReadAsync(buffer, idleCanceller.Token).ConfigureAwait(false);
+                if (length == 0) break;
+
+                // ライターに読み取りサイズを通知
+                writer.Advance(length);
+
+                // トータル読み取りサイズを管理
+                total += length;
+            }
+            catch (OperationCanceledException) when (!cancelToken.IsCancellationRequested)
+            {
+                break;
+            }
+        }
+        return total;
+    }
+}
---
+++ tests/Extensions/TextStreamExtensionsTests.cs
@@ -0,0 +1,135 @@
+﻿using System.Buffers;
+using System.IO.Pipelines;
+
+namespace LestalyTest.Extensions;
+
+[TestClass]
+public class TextStreamExtensionsTests
+{
+    [TestMethod]
+    public async Task ReadToIdleAsync_Array_Full()
+    {
+        // テスト用パイプ
+        var pipe = new Pipe();
+
+        // テスト用データ
+        var dummy = Random.Shared.GetItems<char>("abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ", 5);
+        var pad = '!';
+        var buffer = new char[32].FillBy(pad);
+
+        // テスト用パイプからの読み取りタスクを開始
+        using var output = new StreamReader(pipe.Reader.AsStream());
+        var idleTime = 3000;
+        var readTask = output.ReadToIdleAsync(buffer, idleTime);
+
+        // パイプに書き込み
+        using var input = new StreamWriter(pipe.Writer.AsStream());
+        var writeCount = buffer.Length / dummy.Length + 1;
+        for (var i = 0; i < writeCount; i++)
+        {
+            await input.WriteAsync(dummy);
+            await input.FlushAsync();
+            await Task.Delay(1000);
+        }
+
+        // アイドル時間を超えるよう待機
+        await Task.Delay(idleTime);
+        input.Write(dummy);
+
+        // 読み取りタスク完了を待機
+        var actualSize = await readTask;
+
+        // 期待値
+        var expect = dummy.AsEnumerable().Repetition(writeCount)
+            .Take(buffer.Length)
+            .ToArray();
+
+        // 結果値の評価
+        buffer[..actualSize].Should().Equal(expect);
+        buffer[actualSize..].Should().AllBeEquivalentTo(pad);
+    }
+
+    [TestMethod]
+    public async Task ReadToIdleAsync_Array_Idle()
+    {
+        // テスト用パイプ
+        var pipe = new Pipe();
+
+        // テスト用データ
+        var dummy = Random.Shared.GetItems<char>("abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ", 5);
+        var pad = '!';
+        var buffer = new char[32].FillBy(pad);
+
+        // テスト用パイプからの読み取りタスクを開始
+        using var output = new StreamReader(pipe.Reader.AsStream());
+        var idleTime = 3000;
+        var readTask = output.ReadToIdleAsync(buffer, idleTime);
+
+        // パイプに書き込み
+        using var input = new StreamWriter(pipe.Writer.AsStream());
+        var writeCount = 3;
+        for (var i = 0; i < writeCount; i++)
+        {
+            await input.WriteAsync(dummy);
+            await input.FlushAsync();
+            await Task.Delay(1000);
+        }
+
+        // アイドル時間を超えるよう待機
+        await Task.Delay(idleTime);
+        input.Write(dummy);
+
+        // 読み取りタスク完了を待機
+        var actualSize = await readTask;
+
+        // 期待値
+        var expect = dummy.AsEnumerable().Repetition(writeCount)
+            .Take(buffer.Length)
+            .ToArray();
+
+        // 結果値の評価
+        buffer[..actualSize].Should().Equal(expect);
+        buffer[actualSize..].Should().AllBeEquivalentTo(pad);
+    }
+
+    [TestMethod]
+    public async Task ReadToIdleAsync_Writer()
+    {
+        // テスト用パイプ
+        var pipe = new Pipe();
+
+        // テスト用データ
+        var dummy = Random.Shared.GetItems<char>("abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ", 5);
+        var writer = new ArrayBufferWriter<char>();
+
+        // テスト用パイプからの読み取りタスクを開始
+        using var output = new StreamReader(pipe.Reader.AsStream());
+        var idleTime = 3000;
+        var readTask = output.ReadToIdleAsync(writer, idleTime);
+
+        // パイプに書き込み
+        using var input = new StreamWriter(pipe.Writer.AsStream());
+        var writeCount = 5;
+        for (var i = 0; i < writeCount; i++)
+        {
+            await input.WriteAsync(dummy);
+            await input.FlushAsync();
+            await Task.Delay(1000);
+        }
+
+        // アイドル時間を超えるよう待機
+        await Task.Delay(idleTime);
+        input.Write(dummy);
+
+        // 読み取りタスク完了を待機
+        var actualSize = await readTask;
+
+        // 期待値
+        var expect = dummy.AsEnumerable().Repetition(writeCount)
+            .ToArray();
+
+        // 結果値の評価
+        writer.WrittenMemory.ToArray().Should().Equal(expect);
+    }
+
+}
