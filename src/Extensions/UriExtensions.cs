ecade3ff4ca459ea57a1b705d2c1d190a6bc289b
toras9000 <toras9000@example.com>
2024-03-02 00:56:20 +0900

    Uriに対する拡張メソッド

---
+++ src/Extensions/UriExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Lestaly;
+
+/// <summary>
+/// Uriに対する拡張メソッド
+/// </summary>
+public static class UriExtensions
+{
+    /// <summary>URIの権限セグメントに対するパスで新しいUriを生成する</summary>
+    /// <remarks>要するにサーバ相対パスを指定して新しいUriを作る</remarks>
+    /// <param name="self">基にするURI</param>
+    /// <param name="path">権限セグメントからのパス</param>
+    /// <returns>新しいUri</returns>
+    public static Uri AuthorityRelative(this Uri self, ReadOnlySpan<char> path)
+    {
+        var authority = self.GetLeftPart(UriPartial.Authority);
+        if (string.IsNullOrWhiteSpace(authority)) return new($"{self.GetLeftPart(UriPartial.Scheme)}{path}");
+        if (path.StartsWith("/")) return new($"{authority}{path}");
+        return new($"{authority}/{path}");
+    }
+}
---
+++ tests/Extensions/UriExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Buffers;
+using System.IO.Pipelines;
+
+namespace LestalyTest.Extensions;
+
+[TestClass]
+public class UriExtensionsTests
+{
+    [TestMethod]
+    public void AuthorityRelative_http()
+    {
+        var baseUri = new Uri("http://abc:def@host.server.com/original/path");
+        baseUri.AuthorityRelative("path/to/other").AbsoluteUri.Should().Be("http://abc:def@host.server.com/path/to/other");
+        baseUri.AuthorityRelative("/slashed/other/path").AbsoluteUri.Should().Be("http://abc:def@host.server.com/slashed/other/path");
+    }
+
+    [TestMethod]
+    public void AuthorityRelative_mailto()
+    {
+        var baseUri = new Uri("mailto:user@contoso.com");
+        baseUri.AuthorityRelative("hoge").AbsoluteUri.Should().Be("mailto:hoge");
+    }
+
+}
