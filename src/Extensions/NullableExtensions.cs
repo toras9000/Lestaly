444093e5a4dc573bdada74708a372a4ab8342b02
toras9000 <toras9000@example.com>
2024-08-14 03:29:14 +0900

    Nullable 拡張メソッド追加

---
+++ src/Extensions/NullableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace Lestaly;
+
+/// <summary>
+/// Nullable に対する拡張メソッド
+/// </summary>
+public static class NullableExtensions
+{
+    /// <summary>Nullable{T} で有効値を優先した加算を行う。</summary>
+    /// <remarks>nullのみならばnull維持したいが、有効値があればそちらを優先したい場合用。</remarks>
+    /// <typeparam name="T">数値型</typeparam>
+    /// <param name="self">加算オペランド1</param>
+    /// <param name="other">加算オペランド2</param>
+    /// <returns>オペランドの両方が有効値であればその合計値を、一方のみが有効値であれば有効値を、両方がnullの場合はnullを返却。</returns>
+    public static T? AddPrefer<T>(this T? self, T? other) where T : struct, INumber<T>
+    {
+        if (other.HasValue) return self.HasValue ? self + other : other;
+        return self;
+    }
+}
---
+++ tests/Extensions/NullableExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿namespace LestalyTest.Extensions;
+
+[TestClass]
+public class NullableExtensionsTests
+{
+    [TestMethod]
+    public void AddPrefer()
+    {
+        ((int?)null).AddPrefer(null).Should().BeNull();
+        ((int?)null).AddPrefer((int?)1).Should().Be(1);
+        ((int?)2).AddPrefer(null).Should().Be(2);
+        ((int?)1).AddPrefer((int?)2).Should().Be(3);
+    }
+
+}
