cb6e7b580c2efef6c35e1a94fee96b452c3395db
toras9000 <toras9000@example.com>
2025-04-06 00:27:27 +0900

    LDAP関連の拡張メソッド追加

---
+++ src/Extensions/LdapExtensions.cs
@@ -0,0 +1,204 @@
+﻿using System.Buffers;
+using System.DirectoryServices.Protocols;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lestaly.Extensions;
+
+/// <summary>LDAP関連の拡張メソッド</summary>
+public static class LdapExtensions
+{
+    /// <summary>非同期でディレクトリリクエストを送信する</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="request">リクエスト</param>
+    /// <param name="partialMode">部分取得モード</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>レスポンス</returns>
+    public static Task<DirectoryResponse> SendRequestAsync(this LdapConnection self, DirectoryRequest request, PartialResultProcessing partialMode = PartialResultProcessing.NoPartialResultSupport, CancellationToken cancelToken = default)
+    {
+        var asyncToken = self.BeginSendRequest(request, partialMode, null, null);
+
+        return Task.Run(() =>
+        {
+            using var canceller = cancelToken.Register(() => self.Abort(asyncToken));
+            try
+            {
+                return self.EndSendRequest(asyncToken); ;
+            }
+            catch (ArgumentException)
+            {
+                cancelToken.ThrowIfCancellationRequested();
+                throw;
+            }
+        });
+    }
+
+    /// <summary>識別名(DN)のエントリを検索取得する</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="dn">識別名(DN)</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>取得されたエントリ</returns>
+    public static async Task<SearchResultEntry?> GetEntryAsync(this LdapConnection self, string dn, CancellationToken cancelToken = default)
+    {
+        // Create a search request.
+        var searchReq = new SearchRequest();
+        searchReq.DistinguishedName = dn;
+        searchReq.Scope = SearchScope.Base;
+
+        // Request a search.
+        var searchRsp = await self.SendRequestAsync(searchReq, cancelToken: cancelToken);
+        if (searchRsp.ResultCode != 0) throw new PavedMessageException($"failed to search: {searchRsp.ErrorMessage}");
+        var searchResult = searchRsp as SearchResponse ?? throw new PavedMessageException("unexpected result");
+
+        return searchResult.Entries[0];
+    }
+
+    /// <summary>識別名(DN)のエントリを検索取得する</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="dn">識別名(DN)</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>取得されたエントリ、またはnull</returns>
+    public static async Task<SearchResultEntry?> GetEntryOrDefaultAsync(this LdapConnection self, string dn, CancellationToken cancelToken = default)
+    {
+        try
+        {
+            // Create a search request.
+            var searchReq = new SearchRequest();
+            searchReq.DistinguishedName = dn;
+            searchReq.Scope = SearchScope.Base;
+
+            // Request a search.
+            var searchRsp = await self.SendRequestAsync(searchReq, cancelToken: cancelToken);
+            if (searchRsp.ResultCode != 0) return default;
+            if (searchRsp is not SearchResponse searchResult) return default;
+            if (searchResult.Entries.Count <= 0) return default;
+            return searchResult.Entries[0];
+        }
+        catch { return default; }
+    }
+
+    /// <summary>エントリを作成する</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="dn">識別名(DN)</param>
+    /// <param name="attributes">エントリの属性</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>レスポンス</returns>
+    public static async Task<DirectoryResponse> CreateEntryAsync(this LdapConnection self, string dn, DirectoryAttribute[] attributes, CancellationToken cancelToken = default)
+    {
+        var createEntryReq = new AddRequest();
+        createEntryReq.DistinguishedName = dn;
+        foreach (var attr in attributes)
+        {
+            createEntryReq.Attributes.Add(attr);
+        }
+
+        var createEntryRsp = await self.SendRequestAsync(createEntryReq, cancelToken: cancelToken);
+        if (createEntryRsp.ResultCode != 0) throw new PavedMessageException($"failed to search: {createEntryRsp.ErrorMessage}");
+
+        return createEntryRsp;
+    }
+
+    /// <summary>エントリに属性を追加する</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="dn">識別名(DN)</param>
+    /// <param name="name">属性名</param>
+    /// <param name="values">属性値</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>レスポンス</returns>
+    public static async Task<DirectoryResponse> AddAttributeAsync(this LdapConnection self, string dn, string name, string[] values, CancellationToken cancelToken = default)
+    {
+        var attrModify = new DirectoryAttributeModification();
+        attrModify.Operation = DirectoryAttributeOperation.Add;
+        attrModify.Name = name;
+        attrModify.AddRange(values);
+
+        var addAttrReq = new ModifyRequest();
+        addAttrReq.DistinguishedName = dn;
+        addAttrReq.Modifications.Add(attrModify);
+
+        var addAttrRsp = await self.SendRequestAsync(addAttrReq, cancelToken: cancelToken);
+        if (addAttrRsp.ResultCode != 0) throw new PavedMessageException($"failed to search: {addAttrRsp.ErrorMessage}");
+
+        return addAttrRsp;
+    }
+
+    /// <summary>エントリの属性を置き換える</summary>
+    /// <param name="self">LDAP接続</param>
+    /// <param name="dn">識別名(DN)</param>
+    /// <param name="name">属性名</param>
+    /// <param name="values">属性値</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>レスポンス</returns>
+    public static async Task<DirectoryResponse> ReplaceAttributeAsync(this LdapConnection self, string dn, string name, string[] values, CancellationToken cancelToken = default)
+    {
+        var attrModify = new DirectoryAttributeModification();
+        attrModify.Operation = DirectoryAttributeOperation.Replace;
+        attrModify.Name = name;
+        attrModify.AddRange(values);
+
+        var addAttrReq = new ModifyRequest();
+        addAttrReq.DistinguishedName = dn;
+        addAttrReq.Modifications.Add(attrModify);
+
+        var addAttrRsp = await self.SendRequestAsync(addAttrReq, cancelToken: cancelToken);
+        if (addAttrRsp.ResultCode != 0) throw new PavedMessageException($"failed to search: {addAttrRsp.ErrorMessage}");
+
+        return addAttrRsp;
+    }
+
+    /// <summary>検索結果エントリから指定の属性の値を1つ取得する</summary>
+    /// <param name="self">検索結果エントリ</param>
+    /// <param name="name">属性名</param>
+    /// <returns>属性値の1つ、もしくはnull</returns>
+    public static object? GetAttributeFirstValue(this SearchResultEntry self, string name)
+    {
+        var attr = self.Attributes[name];
+        if (attr == null) return null;
+        if (attr.Count <= 0) return null;
+
+        return attr[0];
+    }
+
+    /// <summary>検索結果エントリから指定の属性の値を列挙する</summary>
+    /// <param name="self">検索結果エントリ</param>
+    /// <param name="name">属性名</param>
+    /// <returns>属性値のシーケンス</returns>
+    public static IEnumerable<object> EnumerateAttributeValues(this SearchResultEntry self, string name)
+    {
+        var attr = self.Attributes[name];
+        if (attr == null) yield break;
+
+        for (var i = 0; i < attr.Count; i++)
+        {
+            yield return attr[i];
+        }
+    }
+
+    /// <summary>パスワードハッシュ化文字列を作成する</summary>
+    /// <param name="password">パスワード文字列</param>
+    /// <param name="salt">salt値。</param>
+    /// <returns>ハッシュ化文字列</returns>
+    public static string MakePasswordHashSSHA256(string password, ReadOnlySpan<byte> salt = default)
+    {
+        // salt を決定。指定されたものがあればそれを、無ければ適当なランダム値を。
+        var randSalt = (stackalloc byte[8]);
+        if (salt.IsEmpty) Random.Shared.NextBytes(randSalt);
+        var useSalt = salt.IsEmpty ? randSalt : salt;
+        // パスワードをUTF8エンコード
+        var buffer = new ArrayBufferWriter<byte>();
+        Encoding.UTF8.GetBytes(password, buffer);
+        // salt を追加
+        buffer.Write(salt);
+        // ハッシュ算出
+        var hashed = SHA256.HashData(buffer.WrittenSpan);
+        // Base64文字列作成
+        buffer.ResetWrittenCount();
+        buffer.Write(hashed);
+        buffer.Write(salt);
+        var encoded = Convert.ToBase64String(buffer.WrittenSpan);
+        // ハッシュ化文字列作成
+        var value = $"{{SSHA256}}{encoded}";
+
+        return value;
+    }
+}
---
+++ tests/Extensions/LdapExtensionsTests.cs
@@ -0,0 +1,182 @@
+﻿using System.DirectoryServices.Protocols;
+using System.Net;
+using Lestaly.Extensions;
+
+namespace LestalyTest.Extensions;
+
+[TestClass]
+public class LdapExtensionsTests
+{
+    public static LdapDirectoryIdentifier? TestServer;
+    public const string RootDn = "dc=myserver,o=home";
+    public const string TestUsersUnitDn = "ou=persons,ou=accounts,dc=myserver,o=home";
+    public const string TestUserDn = $"uid=user1,{TestUsersUnitDn}";
+    public static readonly NetworkCredential ModifierCredential = new("uid=configurator,ou=operators,dc=myserver,o=home", "configurator-pass");
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext)
+    {
+        try
+        {
+            var server = new LdapDirectoryIdentifier("localhost", 389);
+            using var ldap = new LdapConnection(server);
+            ldap.SessionOptions.SecureSocketLayer = false;
+            ldap.SessionOptions.ProtocolVersion = 3;
+            ldap.AuthType = AuthType.Anonymous;
+            ldap.Bind();
+
+            TestServer = server;
+        }
+        catch { }
+    }
+
+    [TestMethod()]
+    public async Task SendRequestAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Anonymous;
+        ldap.Bind();
+
+        var searchReq = new SearchRequest();
+        searchReq.DistinguishedName = "";
+        searchReq.Scope = SearchScope.Base;
+
+        var searchRsp = await ldap.SendRequestAsync(searchReq);
+        searchRsp.Should().BeOfType<SearchResponse>().Which.Entries.Count.Should().NotBe(0); ;
+    }
+
+    [TestMethod()]
+    public async Task SendRequestAsync_Cancel()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Anonymous;
+        ldap.Bind();
+
+        var searchReq = new SearchRequest();
+        searchReq.DistinguishedName = "";
+        searchReq.Scope = SearchScope.Base;
+
+        using var canceller = new CancellationTokenSource();
+        canceller.Cancel();
+        await FluentActions.Awaiting(() => ldap.SendRequestAsync(searchReq, cancelToken: canceller.Token))
+                    .Should().ThrowAsync<OperationCanceledException>();
+    }
+
+    [TestMethod()]
+    public async Task GetEntryAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Anonymous;
+        ldap.Bind();
+
+        var resultEntry = await ldap.GetEntryAsync(TestUserDn);
+        Assert.IsNotNull(resultEntry);
+        resultEntry.DistinguishedName.Should().Be(TestUserDn);
+    }
+
+    [TestMethod()]
+    public async Task GetEntryOrDefaultAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Anonymous;
+        ldap.Bind();
+
+        var resultEntry = await ldap.GetEntryOrDefaultAsync(TestUserDn);
+        Assert.IsNotNull(resultEntry);
+        resultEntry.DistinguishedName.Should().Be(TestUserDn);
+    }
+
+    [TestMethod()]
+    public async Task GetEntryOrDefaultAsync_fail()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Anonymous;
+        ldap.Bind();
+
+        var resultEntry = await ldap.GetEntryOrDefaultAsync("uid=invalid");
+        resultEntry.Should().BeNull();
+    }
+
+    [TestMethod()]
+    public async Task CreateEntryAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Basic;
+        ldap.Credential = ModifierCredential;
+        ldap.Bind();
+
+        var uid = Guid.NewGuid().ToString();
+        var resultEntry = await ldap.CreateEntryAsync($"uid={uid},{TestUsersUnitDn}", [new("objectClass", "account"), new("uid", uid)]);
+        Assert.IsNotNull(resultEntry);
+    }
+
+    [TestMethod()]
+    public async Task AddAttributeAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Basic;
+        ldap.Credential = ModifierCredential;
+        ldap.Bind();
+
+        var uid = Guid.NewGuid().ToString();
+        var dn = $"uid={uid},{TestUsersUnitDn}";
+        await ldap.CreateEntryAsync(dn, [new("objectClass", "account"), new("uid", uid)]);
+        await ldap.AddAttributeAsync(dn, "description", ["test"]);
+        var entry = await ldap.GetEntryOrDefaultAsync(dn);
+        entry.Should().NotBeNull();
+        entry.GetAttributeFirstValue("description").Should().Be("test");
+    }
+
+    [TestMethod()]
+    public async Task ReplaceAttributeAsync()
+    {
+        if (TestServer == null) Assert.Inconclusive();
+
+        using var ldap = new LdapConnection(TestServer);
+        ldap.SessionOptions.ProtocolVersion = 3;
+        ldap.AuthType = AuthType.Basic;
+        ldap.Credential = ModifierCredential;
+        ldap.Bind();
+
+        var uid = Guid.NewGuid().ToString();
+        var dn = $"uid={uid},{TestUsersUnitDn}";
+        await ldap.CreateEntryAsync(dn, [new("objectClass", "account"), new("uid", uid)]);
+        await ldap.ReplaceAttributeAsync(dn, "description", ["test1", "test2"]);
+        var entry = await ldap.GetEntryOrDefaultAsync(dn);
+        entry.Should().NotBeNull();
+        entry.EnumerateAttributeValues("description").Should().Equal("test1", "test2");
+    }
+
+    [TestMethod()]
+    public void MakePasswordHashSSHA256()
+    {
+        var salt = (stackalloc byte[] { 0xCD, 0x22, 0x1C, 0x92, 0xBE, 0x08, 0x52, 0xFE });
+        var pass = "abc";
+        var expect = "{SSHA256}Q/Qu/OcrWcBlpYYLBIZ0DhJnjt4K82fMJfhESavbRq3NIhySvghS/g==";
+        var actual = LdapExtensions.MakePasswordHashSSHA256(pass, salt);
+        actual.Should().Be(expect);
+    }
+
+
+}
