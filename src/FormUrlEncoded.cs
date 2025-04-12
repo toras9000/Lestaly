6e62ab62d254e670ff672b997fbf1fc49f3026d4
toras9000 <toras9000@example.com>
2025-04-12 21:40:05 +0900

    http関係のメソッド追加

---
+++ src/FormUrlEncoded.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Lestaly;
+
+/// <summary>FormUrlEncoded 関係の補助処理</summary>
+public static class FormUrlEncoded
+{
+    /// <summary>オブジェクトから FormUrlEncodedContent を構築する</summary>
+    /// <typeparam name="T">オブジェクト型</typeparam>
+    /// <param name="value">オブジェクトインスタンス</param>
+    /// <returns>構築された FormUrlEncodedContent</returns>
+    public static FormUrlEncodedContent CreateContent<T>(T value)
+    {
+        static IEnumerable<KeyValuePair<string, string>> enumerateMembers(T obj)
+        {
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                var dispAttr = prop.GetCustomAttribute<DisplayAttribute>();
+                var name = dispAttr?.Name ?? prop.Name;
+                var value = prop.GetValue(obj);
+                yield return new(name, $"{value}");
+            }
+        }
+
+        return new FormUrlEncodedContent(enumerateMembers(value));
+    }
+}
---
+++ tests/FormUrlEncodedTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LestalyTest;
+
+[TestClass]
+public class FormUrlEncodedTests
+{
+    public record BaseItem(string Text);
+    public record DerivedItem(string Text, int Value) : BaseItem(Text);
+
+    [TestMethod()]
+    public async Task CreateContent()
+    {
+        var value = new DerivedItem("abc", 123);
+        var content = FormUrlEncoded.CreateContent(value);
+        var contentString = await content.ReadAsStringAsync();
+        contentString.Should().Contain("Text=abc").And.Contain("Value=123");
+    }
+}
