ad1c0a9d1619962bfe75a3b4ed6cc53f5b0f3f60
toras9000 <toras9000@example.com>
2025-05-02 22:18:57 +0900

    環境変数の補助メソッド

---
+++ src/EnvVers.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace Lestaly;
+
+/// <summary>環境変数に関する補助メソッド</summary>
+public static class EnvVers
+{
+    /// <summary>環境変数を文字列で取得する</summary>
+    /// <param name="name">環境変数名</param>
+    /// <returns>環境変数の文字列</returns>
+    public static string? String(string name)
+        => Environment.GetEnvironmentVariable(name);
+
+    /// <summary>環境変数を文字列で取得する</summary>
+    /// <param name="name">環境変数名</param>
+    /// <param name="alt">代替文字列</param>
+    /// <returns>環境変数の文字列</returns>
+    public static string String(string name, string alt)
+        => Environment.GetEnvironmentVariable(name).WhenEmpty(alt);
+
+    /// <summary>環境変数を数値として取得する</summary>
+    /// <typeparam name="TNumber">数値の型</typeparam>
+    /// <param name="name">環境変数名</param>
+    /// <returns>環境変数の数値</returns>
+    public static TNumber? Number<TNumber>(string name) where TNumber : struct, INumber<TNumber>
+        => Environment.GetEnvironmentVariable(name)?.TryParseNumber<TNumber>();
+
+    /// <summary>環境変数を数値として取得する</summary>
+    /// <typeparam name="TNumber">数値の型</typeparam>
+    /// <param name="name">環境変数名</param>
+    /// <param name="alt">代替値</param>
+    /// <returns>環境変数の数値</returns>
+    public static TNumber Number<TNumber>(string name, TNumber alt) where TNumber : struct, INumber<TNumber>
+        => Environment.GetEnvironmentVariable(name)?.TryParseNumber<TNumber>() ?? alt;
+}
+
---
+++ tests/EnvVarsTests.cs
@@ -0,0 +1,45 @@
+﻿namespace LestalyTest;
+
+[TestClass()]
+public class EnvVarsTests
+{
+    [TestMethod()]
+    public void String()
+    {
+        {
+            var varName = $"TEST-{DateTime.Now.Ticks:X}";
+            Environment.SetEnvironmentVariable(varName, "ABCDEF");
+            EnvVers.String(varName).Should().Be("ABCDEF");
+            EnvVers.String(varName + "-MISSING").Should().BeNull();
+        }
+        {
+            var varName = $"TEST-{DateTime.Now.Ticks:X}";
+            Environment.SetEnvironmentVariable(varName, "ABCDEF");
+            EnvVers.String(varName, "XYZ").Should().Be("ABCDEF");
+            EnvVers.String(varName + "-MISSING", "XYZ").Should().Be("XYZ");
+        }
+    }
+
+    [TestMethod()]
+    public void Number()
+    {
+        {
+            var varName = $"TEST-{DateTime.Now.Ticks:X}";
+            Environment.SetEnvironmentVariable(varName, "123");
+            EnvVers.Number<int>(varName).Should().Be(123);
+            EnvVers.Number<int>(varName + "-MISSING").Should().BeNull();
+        }
+        {
+            var varName = $"TEST-{DateTime.Now.Ticks:X}";
+            Environment.SetEnvironmentVariable(varName, "123");
+            EnvVers.Number<int>(varName, 456).Should().Be(123);
+            EnvVers.Number<int>(varName + "-MISSING", 456).Should().Be(456);
+        }
+        {
+            var varName = $"TEST-{DateTime.Now.Ticks:X}";
+            Environment.SetEnvironmentVariable(varName, "ABC");
+            EnvVers.Number<int>(varName, 456).Should().Be(456);
+            EnvVers.Number<int>(varName + "-MISSING", 456).Should().Be(456);
+        }
+    }
+}
