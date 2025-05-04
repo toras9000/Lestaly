7469cc3c46a3b71eb52cf902111e1c209180b009
toras9000 <toras9000@example.com>
2025-05-04 15:47:08 +0900

    キャンセルトークン関連拡張メソッド

---
+++ src/Extensions/CancellationTokenSourceExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Lestaly;
+
+/// <summary>キャンセルトークン関連拡張メソッド</summary>
+public static class CancellationTokenSourceExtensions
+{
+    /// <summary>指定時間でタイムアウトするリンクトークンソースを作成する</summary>
+    /// <param name="self">リンク対象のトークン</param>
+    /// <param name="timeout">タイムアウト時間</param>
+    /// <returns>リンクトークンソース</returns>
+    public static CancellationTokenSource CreateLink(this CancellationToken self, TimeSpan timeout)
+    {
+        var linkSource = CancellationTokenSource.CreateLinkedTokenSource(self);
+        linkSource.CancelAfter(timeout);
+        return linkSource;
+    }
+
+    /// <summary>リンクトークンソースを作成する</summary>
+    /// <param name="self">リンク対象のトークン</param>
+    /// <param name="token">リンクするトークン</param>
+    /// <returns>リンクトークンソース</returns>
+    public static CancellationTokenSource CreateLink(this CancellationToken self, CancellationToken token)
+        => CancellationTokenSource.CreateLinkedTokenSource(self, token);
+
+    /// <summary>リンクトークンソースを作成する</summary>
+    /// <param name="self">リンク対象のトークン</param>
+    /// <param name="token1">リンクするトークン1</param>
+    /// <param name="token2">リンクするトークン2</param>
+    /// <returns>リンクトークンソース</returns>
+    public static CancellationTokenSource CreateLink(this CancellationToken self, CancellationToken token1, CancellationToken token2)
+        => CancellationTokenSource.CreateLinkedTokenSource(self, token1, token2);
+}
---
+++ tests/Extensions/CancellationTokenSourceExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿namespace LestalyTest.Extensions;
+
+[TestClass]
+public class CancellationTokenSourceExtensionsTests
+{
+    [TestMethod]
+    public async Task CreateLink()
+    {
+        {
+            using var breaker = new CancellationTokenSource();
+            using var linked = breaker.Token.CreateLink(TimeSpan.FromMilliseconds(500));
+            await FluentActions.Awaiting(() => Task.Delay(1000, linked.Token)).Should()
+                .ThrowAsync<OperationCanceledException>()
+                .Where(ex => ex.CancellationToken.Equals(linked.Token));
+        }
+
+        {
+            using var source1 = new CancellationTokenSource();
+            using var source2 = new CancellationTokenSource();
+            using var linked = source1.Token.CreateLink(source2.Token);
+            source2.Cancel();
+            await FluentActions.Awaiting(() => Task.Delay(1000, linked.Token)).Should()
+                .ThrowAsync<OperationCanceledException>()
+                .Where(ex => ex.CancellationToken.Equals(linked.Token));
+        }
+    }
+}
