7fd0c4df85ebb0574fb4da9a4cd84c56a1a87233
toras9000 <toras9000@example.com>
2025-07-09 01:27:16 +0900

    セマフォ関連処理

---
+++ src/Lestaly.General/SemaphoreFeeder.cs
@@ -0,0 +1,96 @@
+﻿namespace Lestaly;
+
+/// <summary>セマフォのカウンタをDisposableオブジェクトを扱うクラス</summary>
+public class SemaphoreFeeder : IDisposable
+{
+    // 構築
+    #region コンストラクタ
+    /// <summary>指定したパラメータでセマフォを初期化するコンストラクタ</summary>
+    /// <param name="initialCount">セマフォの初期カウント値</param>
+    public SemaphoreFeeder(int initialCount)
+    {
+        this.semaphore = new SemaphoreSlim(initialCount);
+    }
+
+    /// <summary>指定したパラメータでセマフォを初期化するコンストラクタ</summary>
+    /// <param name="initialCount">セマフォの初期カウント値</param>
+    /// <param name="maxCount">セマフォの最大カウント値</param>
+    public SemaphoreFeeder(int initialCount, int maxCount)
+    {
+        this.semaphore = new SemaphoreSlim(initialCount, maxCount);
+    }
+    #endregion
+
+    // 公開メソッド
+    #region カウンタ取得
+    /// <summary>セマフォカウントの取得を待機し、解放トークンを得る</summary>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>セマフォカウントの解放トークン</returns>
+    public async ValueTask<IDisposable> WaitAsync(CancellationToken cancelToken = default)
+    {
+        return await Token.WaitAsync(this, cancelToken);
+    }
+
+    /// <summary>セマフォカウントの取得を待機し、解放トークンを得る</summary>
+    /// <param name="timeout">取得タイムアウト時間</param>
+    /// <param name="cancelToken">キャンセルトークン</param>
+    /// <returns>カウント取得できた場合は解放トークン、取得できなかった場合はnull</returns>
+    public async ValueTask<IDisposable?> WaitAsync(TimeSpan timeout, CancellationToken cancelToken = default)
+    {
+        return await Token.WaitAsync(this, timeout, cancelToken);
+    }
+    #endregion
+
+    #region 破棄
+    /// <summary>リソースを破棄する</summary>
+    public void Dispose()
+    {
+        this.semaphore.Dispose();
+    }
+    #endregion
+
+    // 非公開型
+    #region カウンタトークン
+    /// <summary>セマフォカウンタの解放トークンとして機能するDisposable</summary>
+    /// <param name="outer">トークン生成元インスタンス</param>
+    private class Token(SemaphoreFeeder outer) : IDisposable
+    {
+        /// <summary>セマフォカウントの取得を待機し、解放トークンを生成する</summary>
+        /// <param name="outer">トークン生成元インスタンス</param>
+        /// <param name="cancelToken">キャンセルトークン</param>
+        /// <returns>セマフォカウントの解放トークン</returns>
+        public static async ValueTask<Token> WaitAsync(SemaphoreFeeder outer, CancellationToken cancelToken)
+        {
+            await outer.semaphore.WaitAsync(cancelToken);
+            return new Token(outer);
+        }
+
+        /// <summary>セマフォカウントの取得を待機し、解放トークンを生成する</summary>
+        /// <param name="outer">トークン生成元インスタンス</param>
+        /// <param name="timeout">取得タイムアウト時間</param>
+        /// <param name="cancelToken">キャンセルトークン</param>
+        /// <returns>カウント取得できた場合は解放トークン、取得できなかった場合はnull</returns>
+        public static async ValueTask<Token?> WaitAsync(SemaphoreFeeder outer, TimeSpan timeout, CancellationToken cancelToken)
+        {
+            var entere = await outer.semaphore.WaitAsync(timeout, cancelToken);
+            return entere ? new Token(outer) : default;
+        }
+
+        /// <summary>リソースを破棄する</summary>
+        public void Dispose()
+        {
+            if (outer != null)
+            {
+                outer.semaphore.Release();
+                outer = null!;
+            }
+        }
+    }
+    #endregion
+
+    // 非公開フィールド
+    #region リソース
+    /// <summary></summary>
+    private readonly SemaphoreSlim semaphore;
+    #endregion
+}
---
+++ tests/SemaphoreFeederTests.cs
@@ -0,0 +1,57 @@
+﻿namespace LestalyTest;
+
+[TestClass]
+public class SemaphoreFeederTests
+{
+    [TestMethod]
+    public async Task WaitAsync_sequential()
+    {
+        using var feeder = new SemaphoreFeeder(1);
+
+        using (var token = await feeder.WaitAsync()) { }
+        using (var token = await feeder.WaitAsync()) { }
+    }
+
+    [TestMethod]
+    public async Task WaitAsync_nest()
+    {
+        using var gate1 = new SemaphoreSlim(0);
+        using var gate2 = new SemaphoreSlim(0);
+        using var feeder = new SemaphoreFeeder(1);
+        var list = new List<int>();
+
+        var task1 = Task.Run(async () =>
+        {
+            using var token = await feeder.WaitAsync();
+            await gate1.WaitAsync();
+            list.Add(1);
+        });
+
+        await Task.Delay(100);
+
+        var task2 = Task.Run(async () =>
+        {
+            using var token = await feeder.WaitAsync();
+            await gate2.WaitAsync();
+            list.Add(2);
+        });
+
+        gate2.Release();
+        await Task.Delay(100);
+        gate1.Release();
+
+        await Task.WhenAll(task1, task2);
+        list.Should().Equal(1, 2);
+    }
+
+    [TestMethod]
+    public async Task WaitAsync_timeout()
+    {
+        using var feeder = new SemaphoreFeeder(1);
+
+        using var token = await feeder.WaitAsync();
+
+        using var nested = await feeder.WaitAsync(TimeSpan.FromMilliseconds(100));
+        nested.Should().BeNull();
+    }
+}
