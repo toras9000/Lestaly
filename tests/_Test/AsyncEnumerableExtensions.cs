42f4dd5ff5b37fe3a85cb78bdd118844bc87f703
toras9000 <toras9000@example.com>
2025-11-13 00:25:18 +0900

    競合メソッドの修正
    - .NET10でIAsyncEnumerable系が標準実装されたので System.Linq.Async と競合
    -  System.Linq.Async の利用をやめる。

---
+++ tests/_Test/AsyncEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace LestalyTest;
+
+public static class AsyncEnumerableExtensions
+{
+#if !NET10_0_OR_GREATER
+    public static async Task<T[]> ToArrayAsync<T>(this IAsyncEnumerable<T> self)
+    {
+        var list = new List<T>();
+        await foreach (var item in self)
+        {
+            list.Add(item);
+        }
+        return list.ToArray();
+    }
+#endif
+}
