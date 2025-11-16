d9a1c02d2575f54cfe9d08a74ddb5488f611f773
toras9000 <toras9000@example.com>
2025-11-16 19:31:11 +0900

    ファイル情報拡張メソッド追加

---
+++ src/Lestaly.General/Cx/CmdFileInfoExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lestaly.Cx;
+
+/// <summary>
+/// コマンドをシンプルに実行するための拡張メソッド
+/// </summary>
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "このクラスでは意図的に小文字メソッドを使っている")]
+public static class CmdFileInfoExtensions
+{
+    extension(FileInfo self)
+    {
+        /// <summary>ファイルを実行ファイルとみなしてプロセスを実行する</summary>
+        /// <returns>コマンドの標準出力と標準エラーのテキストを得るAwaiter。出力テキストは正常終了コードの場合のみ得られる。</returns>
+        public TaskAwaiter<string> GetAwaiter()
+            => new CmdCx(self.FullName).result().success().output().GetAwaiter();
+
+        /// <summary>文字列をコマンドラインとみなしてプロセスを実行する</summary>
+        /// <returns>コマンドの実行結果(出力と終了コード)を得るタスク。終了コードは検証されない。</returns>
+        public Task<CmdResult> launch()
+            => new CmdCx(self.FullName).result();
+    }
+}
+
---
+++ tests/Cx/CmdFileInfoExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Lestaly.Cx;
+
+namespace LestalyTest.Cx;
+
+[TestClass()]
+public class CmdFileInfoExtensionsTests
+{
+    [TestMethod()]
+    public async Task GetAwaiter()
+    {
+        var ipcnofigExe = Environment.SpecialFolder.System.GetInfo().RelativeFile("ipconfig.exe");
+        (await ipcnofigExe).Should().Contain("DNS");
+    }
+
+    [TestMethod()]
+    public async Task launch()
+    {
+        var ipcnofigExe = Environment.SpecialFolder.System.GetInfo().RelativeFile("ipconfig.exe");
+        (await ipcnofigExe.launch().output()).Should().Contain("DNS");
+    }
+}
