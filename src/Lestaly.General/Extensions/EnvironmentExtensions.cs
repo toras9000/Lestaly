da7565bb7b98482da16d6dffad35eb8b50f4b9fc
toras9000 <toras9000@example.com>
2025-11-16 19:34:34 +0900

    特殊ディレクトリ関連変更

--- src/Lestaly.General/Extensions/EnvironmentExtensions.cs
+++ src/Lestaly.General/Extensions/EnvironmentExtensions.cs
@@ -5,20 +5,16 @@
 /// </summary>
 public static class EnvironmentExtensions
 {
-    /// <summary>特殊フォルダを示すディレクトリ情報を取得する</summary>
-    /// <param name="self">特殊フォルダ種別</param>
-    /// <returns>ディレクトリ情報</returns>
-    public static DirectoryInfo GetInfo(this Environment.SpecialFolder self)
+    // 特殊フォルダ関連の拡張メソッド
+    extension(Environment.SpecialFolder self)
     {
-        return new DirectoryInfo(Environment.GetFolderPath(self));
-    }
+        /// <summary>指定の種別の特殊ディレクトリ情報を取得する。</summary>
+        /// <returns>ディレクトリ情報</returns>
+        public DirectoryInfo GetInfo() => new(Environment.GetFolderPath(self));
 
-    /// <summary>特殊フォルダを示すディレクトリ情報を取得する</summary>
-    /// <param name="self">特殊フォルダ種別</param>
-    /// <param name="option">取得オプション</param>
-    /// <returns>ディレクトリ情報</returns>
-    public static DirectoryInfo GetInfo(this Environment.SpecialFolder self, Environment.SpecialFolderOption option)
-    {
-        return new DirectoryInfo(Environment.GetFolderPath(self, option));
+        /// <summary>特殊フォルダを示すディレクトリ情報を取得する</summary>
+        /// <param name="option">取得オプション</param>
+        /// <returns>ディレクトリ情報</returns>
+        public DirectoryInfo GetInfo(Environment.SpecialFolderOption option) => new(Environment.GetFolderPath(self, option));
     }
 }
--- src/Lestaly.General/SpecialFolder.cs
+++ src/Lestaly.General/SpecialFolder.cs
@@ -5,18 +5,13 @@
 /// </summary>
 public static class SpecialFolder
 {
-    /// <summary>指定の種別の特殊ディレクトリ情報を取得する。</summary>
-    /// <param name="kind">特殊フォルダ種別</param>
-    /// <returns>ディレクトリ情報</returns>
-    public static DirectoryInfo Get(Environment.SpecialFolder kind) => new(Environment.GetFolderPath(kind));
-
     /// <summary>ユーザプロファイルディレクトリ情報を取得する。</summary>
     /// <returns>ディレクトリ情報</returns>
-    public static DirectoryInfo UserProfile() => Get(Environment.SpecialFolder.UserProfile);
+    public static DirectoryInfo UserProfile() => Environment.SpecialFolder.UserProfile.GetInfo();
 
     /// <summary>現在のユーザのアプリケーションデータディレクトリ情報を取得する。</summary>
     /// <returns>ディレクトリ情報</returns>
-    public static DirectoryInfo ApplicationData() => Get(Environment.SpecialFolder.ApplicationData);
+    public static DirectoryInfo ApplicationData() => Environment.SpecialFolder.ApplicationData.GetInfo();
 
     /// <summary>テンポラリディレクトリ情報を取得する。</summary>
     /// <returns>ディレクトリ情報</returns>
