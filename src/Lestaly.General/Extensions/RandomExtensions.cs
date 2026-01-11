b526f186a1e97ff5e68205f74b7aac6cacae0a7d
toras9000 <toras9000@example.com>
2026-01-11 14:16:38 +0900

    ランダム関係の拡張メソッド

--- src/Lestaly.General/Extensions/RandomExtensions.cs
+++ src/Lestaly.General/Extensions/RandomExtensions.cs
@@ -1,8 +1,23 @@
 ﻿namespace Lestaly;
 
-/// <summary>
-/// Random に対する拡張メソッド
-/// </summary>
+/// <summary>重み付き選択肢データ型</summary>
+/// <typeparam name="TOption">値の型</typeparam>
+/// <param name="Value">値</param>
+/// <param name="Weight">重み</param>
+public readonly record struct OptionWeight<TOption>(TOption Value, ushort Weight);
+
+/// <summary>重み付き選択肢データ型のヘルパ型</summary>
+public static class OptionWeight
+{
+    /// <summary>重み付き選択肢データ型</summary>
+    /// <typeparam name="TOption">値の型</typeparam>
+    /// <param name="value">値</param>
+    /// <param name="weight">重み</param>
+    /// <returns>重み付き選択肢データ</returns>
+    public static OptionWeight<TOption> Create<TOption>(TOption value, ushort weight) => new(value, weight);
+}
+
+/// <summary>Random に対する拡張メソッド</summary>
 public static class RandomExtensions
 {
     /// <summary>ランダム値のバイト配列を作成する。</summary>
@@ -15,4 +30,74 @@ public static class RandomExtensions
         self.NextBytes(data);
         return data;
     }
+
+    /// <summary>コレクションからランダムに1つの要素を取得する</summary>
+    /// <typeparam name="TItem">要素の型</typeparam>
+    /// <param name="self">ランダム値を生成するインスタンス</param>
+    /// <param name="items">対象コレクション</param>
+    /// <returns>ランダムに選択された要素</returns>
+    public static TItem Pick<TItem>(this Random self, IReadOnlyList<TItem> items)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+        var index = self.Next(items.Count);
+        return items[index];
+    }
+
+    /// <summary>コレクションからランダムに重複無しで指定数の要素を取得する</summary>
+    /// <typeparam name="TItem">要素の型</typeparam>
+    /// <param name="self">ランダム値を生成するインスタンス</param>
+    /// <param name="items">対象コレクション</param>
+    /// <param name="count">取得数。ただしコレクション長でキャップされる。</param>
+    /// <returns>ランダムに取得された要素の配列</returns>
+    public static TItem[] PickItems<TItem>(this Random self, IReadOnlyList<TItem> items, int count)
+    {
+        ArgumentNullException.ThrowIfNull(items);
+        ArgumentOutOfRangeException.ThrowIfNegative(count);
+
+        if (count == 0) return Array.Empty<TItem>();
+        var pickCount = Math.Min(count, items.Count);
+        var picked = new TItem[pickCount];
+        var used = new SortedSet<int>();
+        var restCount = items.Count;
+        for (var i = 0; i < pickCount; i++)
+        {
+            var index = self.Next(restCount);
+            foreach (var usedIdx in used)
+            {
+                if (usedIdx <= index) index++;
+                else break;
+            }
+            picked[i] = items[index];
+            used.Add(index);
+            restCount--;
+        }
+        return picked;
+    }
+
+    /// <summary>重みづけした選択肢の中からランダムで1つ値を選択する</summary>
+    /// <typeparam name="TOption">選択肢データ型</typeparam>
+    /// <param name="self">ランダム値を生成するインスタンス</param>
+    /// <param name="options">重みづけされた選択肢のスパン</param>
+    /// <returns>ランダムに選択された値</returns>
+    public static TOption Choice<TOption>(this Random self, params ReadOnlySpan<OptionWeight<TOption>> options)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(options.Length);
+
+        var total = 0;
+        for (var i = 0; i < options.Length; i++)
+        {
+            total = checked(total + options[i].Weight);
+        }
+
+        var amp = self.Next(total);
+        for (var i = 0; i < options.Length; i++)
+        {
+            var item = options[i];
+            if (amp < item.Weight) return item.Value;
+            amp -= item.Weight;
+        }
+
+        return options[^1].Value;
+    }
+
 }
---
+++ tests/Extensions/RandomExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿namespace LestalyTest.Extensions;
+
+[TestClass()]
+public class RandomExtensionsTests
+{
+    [TestMethod()]
+    public void GetBytes()
+    {
+        Random.Shared.GetBytes(10).Should().HaveCount(10);
+    }
+
+    [TestMethod()]
+    public void Pick()
+    {
+        var items = Enumerable.Range(1, 200).Select(n => n * 3).ToArray();
+        Random.Shared.Pick(items).Should().BeOneOf(items);
+        Random.Shared.Pick(items).Should().BeOneOf(items);
+        Random.Shared.Pick(items).Should().BeOneOf(items);
+    }
+
+    [TestMethod()]
+    public void PickItems()
+    {
+        var items = Enumerable.Range(1, 100).Select(n => n * 3).ToArray();
+
+        Random.Shared.PickItems(items, 10).Should().HaveCount(10).And.BeSubsetOf(items).And.OnlyHaveUniqueItems();
+        Random.Shared.PickItems(items, 99).Should().HaveCount(99).And.BeSubsetOf(items).And.OnlyHaveUniqueItems();
+        Random.Shared.PickItems(items, 100).Should().HaveCount(100).And.BeSubsetOf(items).And.OnlyHaveUniqueItems();
+        Random.Shared.PickItems(items, 101).Should().HaveCount(100).And.BeSubsetOf(items).And.OnlyHaveUniqueItems();
+    }
+
+    [TestMethod()]
+    public void Choice()
+    {
+        var options = new[]
+        {
+            OptionWeight.Create(DateTimeKind.Unspecified, 10),
+            OptionWeight.Create(DateTimeKind.Utc, 20),
+            OptionWeight.Create(DateTimeKind.Local, 30),
+        };
+
+        var choices = Enumerable.Range(0, 10000)
+            .Select(_ => Random.Shared.Choice(options))
+            .ToLookup(v => v);
+        choices.Count.Should().Be(3);
+
+        var unspecCount = choices[DateTimeKind.Unspecified].Count();
+        var utfCount = choices[DateTimeKind.Utc].Count();
+        var localCount = choices[DateTimeKind.Local].Count();
+        unspecCount.Should().NotBe(0).And.BeLessThan(utfCount).And.BeLessThan(localCount);
+        utfCount.Should().NotBe(0).And.BeLessThan(localCount);
+        localCount.Should().NotBe(0);
+    }
+
+}
