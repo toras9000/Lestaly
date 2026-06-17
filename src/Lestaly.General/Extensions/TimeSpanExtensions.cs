0f394a509ebb4d2e1146ebf145bca4b6b3f9d39f
toras9000 <toras9000@example.com>
2026-06-17 20:20:59 +0900

    TimeSpan拡張メソッド

---
+++ src/Lestaly.General/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Lestaly;
+
+/// <summary>TimeSpan の拡張メンバ</summary>
+public static class TimeSpanExtensions
+{
+    /// <summary>TimeSpan のインスタンス拡張メンバ</summary>
+    extension(TimeSpan self)
+    {
+        /// <summary>シンプルな表現(hhhhhh:mm:ss)で文字列化する</summary>
+        /// <returns>文字列表現</returns>
+        public string ToSimple()
+            => $"{(long)self.TotalHours}:{self.Minutes:D2}:{self.Seconds:D2}";
+    }
+}
---
+++ tests/Extensions/TimeSpanExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace LestalyTest.Extensions;
+
+[TestClass]
+public class TimeSpanExtensionsTests
+{
+    [TestMethod]
+    public void ToSimple()
+    {
+        new TimeSpan(1, 2, 3).ToSimple().Should().Be("1:02:03");
+        new TimeSpan(12, 23, 34).ToSimple().Should().Be("12:23:34");
+        new TimeSpan(234, 34, 45).ToSimple().Should().Be("234:34:45");
+    }
+}
